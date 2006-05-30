d6c3328d977b37015a5b78b7aabb2fe3e571c165
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-05-30 08:25:35 +0000

    setTerms added

--- iris/trunk/api/org/deri/iris/api/basics/ITuple.java
+++ iris/trunk/api/org/deri/iris/api/basics/ITuple.java
@@ -17,6 +17,26 @@ public interface ITuple {
 	
 	public List<ITerm> getTerms();
 	
+	public boolean setTerm(int index, ITerm term);
+	
+	/**
+	 * Inserts all of the elements in the specified collection into this 
+	 * list at the specified position. Shifts 
+	 * the element currently at that position (if any) and any subsequent 
+	 * elements to the right (increases their indices). The new elements 
+	 * will appear in this list in the order that they are returned by 
+	 * the specified collection's iterator. This method is thread-save.
+	 * 
+	 * @param index
+	 * 				- index at which to insert first element from 
+	 * 				the specified collection.
+	 * @param terms
+	 * 				- elements to be inserted into this list.
+	 * @return true 
+	 * 				if this list changed as a result of the call.
+	 */
+	public boolean setTerms(int index, List<ITerm> terms);
+	
 	public boolean equals(Object o);
 	
 	public String toString();
