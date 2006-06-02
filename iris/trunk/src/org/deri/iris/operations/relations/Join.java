ce53f106e07c8d934f9f9febaaad0b8999505f99
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-06-02 13:44:46 +0000

    bug fix

--- iris/trunk/src/org/deri/iris/operations/relations/Join.java
+++ iris/trunk/src/org/deri/iris/operations/relations/Join.java
@@ -20,19 +20,22 @@
 
 package org.deri.iris.operations.relations;
 
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.SortedSet;
 
 import org.deri.iris.api.basics.ITuple;
 import org.deri.iris.api.operations.relation.IJoin;
 import org.deri.iris.api.storage.IRelation;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.basics.Tuple;
+import org.deri.iris.operations.tuple.BasicComparator;
 import org.deri.iris.operations.tuple.Concatenation;
-import org.deri.iris.operations.tuple.TupleComparator;
+import org.deri.iris.operations.tuple.IndexComparator;
+import org.deri.iris.operations.tuple.JoinComparator;
 import org.deri.iris.storage.Relation;
 
-//TODO: indexes.length*2 should be replaced be r0.size() + r1.size() (what if
-//joining a relation of arity 2 to a relation of arity 6?)
 /**
  * @author Darko Anicic, DERI Innsbruck
  * @date   24.05.2006 09:26:43
@@ -41,52 +44,56 @@ public class Join implements IJoin{
 	
 	private IRelation relation0 = null;
 	private IRelation relation1 = null;
-	private TupleComparator comparator = null;
+	private BasicComparator comparator = null;
+	private int[] indexes = null;
+	
 	private JoinCondition condition = JoinCondition.EQUALS;
 	
 	public IRelation join(IRelation relation0, IRelation relation1, int[] indexes, JoinCondition condition) {
 		this.condition = condition;
-		return join(relation0, relation1, indexes);
+		this.indexes = indexes;
+		return join(relation0, relation1);
 	}
 	
 	public IRelation equiJoin(IRelation arg0, IRelation arg1, int[] indexes) {
 		this.condition = JoinCondition.EQUALS;
-		return join(relation0, relation1, indexes);
+		this.indexes = indexes;
+		return join(relation0, relation1);
 	}
 	
 	@SuppressWarnings("unchecked")
-	private IRelation join(IRelation relation0, IRelation relation1, int[] indexes) {
+	private IRelation join(IRelation relation0, IRelation relation1) {
 		if (((ITuple)relation0.first()).getArity() != ((ITuple)relation1.first()).getArity()) {
 			throw new IllegalArgumentException("Couldn't join due to different arity of tuples.");
 		} 
-		this.comparator = new TupleComparator(indexes);
-		IRelation joinRelation = new Relation(indexes.length*2);
+		IRelation joinRelation = new Relation(this.indexes.length*2);
 		
-		if(!Arrays.equals(((TupleComparator)relation0.comparator()).getSortIndexes(), indexes)){
-			this.relation0 = new Relation(comparator);
-		}
+		// Sort relation0 on those tupples defined by sort indexes
+		this.comparator = new IndexComparator(this.indexTransformer0(indexes));
+		this.relation0 = new Relation(comparator);
 		this.relation0.addAll(relation0);
-		if(!Arrays.equals(((TupleComparator)relation1.comparator()).getSortIndexes(), indexes)){
-			this.relation1 = new Relation(comparator);
-		}
+	
+		// Sort relation1 on those tupples defined by sort indexes
+		this.comparator = new IndexComparator(this.indexTransformer1(indexes));
+		this.relation1 = new Relation(comparator);
 		this.relation1.addAll(relation1);
 		
 		Iterator<ITuple> iterator;
 		ITuple tuple;
 		boolean order = false;
 		
-		if(relation0.size() < relation1.size()){
+		if(this.relation0.size() < this.relation1.size()){
 			order = true;
-			iterator = relation0.iterator();
+			iterator = this.relation0.iterator();
 			while(iterator.hasNext()){
 				tuple = iterator.next();
-				joinRelation.addAll(findAndJoin(relation1, tuple, indexes, order));
+				joinRelation.addAll(findAndJoin(tuple, order));
 			}
 		}else{
-			iterator = relation1.iterator();
+			iterator = this.relation1.iterator();
 			while(iterator.hasNext()){
 				tuple = iterator.next();
-				joinRelation.addAll(findAndJoin(relation0, tuple, indexes, order));
+				joinRelation.addAll(findAndJoin(tuple, order));
 			}
 		}
 			
@@ -94,70 +101,171 @@ public class Join implements IJoin{
 	}
 	
 	@SuppressWarnings("unchecked")
-	private IRelation findAndJoin(IRelation relation, ITuple tuple, int[] indexes, 
-			boolean order){
+	private IRelation findAndJoin(ITuple tuple, boolean order){
 		
-		IRelation joinElements = new Relation(indexes.length*2);
+		if(relation1.first() == null) return null;
+		IRelation joinElements = 
+			new Relation(((ITuple)relation1.first()).getArity()*2);
 		Concatenation concatenator = new Concatenation();
 		SortedSet subSet = null;
 		
+		ITuple transformedTuple;
 		if(order){
-			subSet = relation.tailSet(tuple);
+			transformedTuple = this.transformTuple1(tuple, indexes);
+			subSet = relation1.tailSet(transformedTuple);
 		}else{
-			subSet = relation.subSet(relation.first(), tuple);
+			transformedTuple = this.transformTuple0(tuple, indexes);
+			subSet = relation0.tailSet(transformedTuple);
 		}
-		Iterator<ITuple> iterator = subSet.iterator(); 
+		
 		ITuple tmpTuple, concatenatedTuple = null;
 		
-		while(iterator.hasNext()){
+		/*
+		 * No duplicates handled
+		*/
+		Iterator<ITuple> iterator = subSet.iterator(); 
+		JoinComparator comparator = new JoinComparator(this.indexes);
+		boolean checkNext = true;
+		int comparison = 0;
+		
+		while(checkNext && iterator.hasNext()){
 			tmpTuple = iterator.next();
 			if(order){
-				if(isConditionSatisfied(tuple, tmpTuple, this.condition)){
-					//create joinTuple = tuple + tmpTuple
-					concatenatedTuple = concatenator.concatenate(tuple, tmpTuple);
-					joinElements.add(concatenatedTuple);
-				}
+				comparison = comparator.compare(tuple, tmpTuple);
+			}else{
+				comparison = comparator.compare(tmpTuple, tuple);
 			}
-			else{
-				if(isConditionSatisfied(tmpTuple, tuple, this.condition)){
-					//create joinTuple = tuple + tmpTuple
-					concatenatedTuple = concatenator.concatenate(tmpTuple, tuple);
+			switch(isConditionSatisfied(comparison)){
+				case -1:
+					if(order)checkNext = false;
+					break;
+				case 0:
+					// create joinTuple = tuple + tmpTuple
+					if(order){
+						concatenatedTuple = concatenator.concatenate(tuple, tmpTuple);
+					}else{
+						concatenatedTuple = concatenator.concatenate(tmpTuple, tuple);
+					}
 					joinElements.add(concatenatedTuple);
-				}
+					break;
+				case 1:
+					if(!order)checkNext = false;
+					break;
 			}
 		}
+		
+		/*
+		 * Duplicates handling
+		*/
+		/*ITuple joinTuple = (ITuple)subSet.first();
+		while(joinTuple != null){
+			// Correct it! (Carefull)
+			if(!joinTuple.equals(transformedTuple)){
+				concatenatedTuple = concatenator.concatenate(tuple, joinTuple);
+			}
+			joinElements.add(concatenatedTuple);
+			joinTuple = joinTuple.getDuplicate();
+		}*/
+		
 		return joinElements;
 	}
 	
-	private boolean isConditionSatisfied(ITuple t0, ITuple t1, JoinCondition condition){
+	/*
+	 * if boolean order = true then 
+	 * isConditionSatisfied = 
+	 * -1	means relevant part of the tree is handled, stop further checking
+	 *  0	means join condition is satisfied for the two compared tupels
+	 *  1	means join condition is not satisfied for the two compared tupels
+	 *  
+	 * if boolean order = flase then
+	 * isConditionSatisfied = 
+	 *  -1	means join condition is not satisfied for the two compared tupels
+	 *  0	means join condition is satisfied for the two compared tupels
+	 *  1	means relevant part of the tree is handled, stop further checking
+	*/
+	private int isConditionSatisfied(int comparison){
 		switch (condition){
 			case EQUALS:
-				if(comparator.joinCompare(t0, t1) == 0)
-					return true;
+				if(comparison == 0) return 0;
+				if(comparison < 0) return -1;
 				break;
 			case NOT_EQUAL:
-				if(comparator.joinCompare(t0, t1) != 0)
-					return true;
+				if(comparison != 0) return 0;
+				//if(comparison < 0)return -1;
 				break;
 			case LESS_THAN:
-				if(comparator.joinCompare(t0, t1) < 0)
-					return true;
+				if(comparison < 0) return 0;
+				if(comparison > 0) return -1;
 				break;
 			case GREATER_THAN:
-				if(comparator.joinCompare(t0, t1) > 0)
-					return true;
+				if(comparison > 0) return 0;
+				if(comparison < 0) return -1;
 				break;
 			case LESS_OR_EQUAL:
-				if(comparator.joinCompare(t0, t1) < 0 ||
-						comparator.joinCompare(t0, t1) == 0)
-					return true;
+				if(comparison < 0 || comparison == 0) return 0;
+				if(comparison > 0) return -1;
 				break;
 			case GREATER_OR_EQUAL:
-				if(comparator.joinCompare(t0, t1) > 0 ||
-						comparator.joinCompare(t0, t1) == 0)
-					return true;
+				if(comparison > 0 || comparison == 0) return 0;
+				if(comparison < 0) return -1;
+				break;
 		}
-		return false;
+		return 1;
 	}
-
+	
+	private int[] indexTransformer0(int[] indexes){
+		int[] tranformedIndexes = new int [indexes.length];
+		for(int i=0; i<indexes.length; i++){
+			if(indexes[i] != -1)tranformedIndexes[i]= i;
+			else tranformedIndexes[i] = -1;
+		}
+		return tranformedIndexes;
+	}
+	
+	private int[] indexTransformer1(int[] indexes){
+		int[] tranformedIndexes = new int [indexes.length];
+		for(int i=0; i<indexes.length; i++){
+			if(indexes[i] != -1){
+				tranformedIndexes[indexes[i]]=indexes[i];
+				tranformedIndexes[i] = -1;
+			}else if(tranformedIndexes[i] == 0)
+				tranformedIndexes[i] = -1;
+		}
+		return tranformedIndexes;
+	}
+	
+	// Refactor it!
+	private ITuple transformTuple0(ITuple tuple, int[] indexes){
+		ITerm[] terms = new ITerm[tuple.getArity()];
+		List<ITerm> termList = new LinkedList();
+		for(int i=0; i<indexes.length; i++){
+			if(indexes[i] != -1)
+				terms[i] = tuple.getTerm(indexes[i]);
+		}		
+		for(int j=0; j<indexes.length; j++){
+			if(terms[j] == null)
+				terms[j] = tuple.getTerm(j).getMinValue();
+		}
+		for(int i=0; i<tuple.getArity(); i++)
+			termList.add(terms[i]);
+		return new Tuple(termList);
+	}
+	
+	// Refactor it!
+	private ITuple transformTuple1(ITuple tuple, int[] indexes){
+		ITerm[] terms = new ITerm[tuple.getArity()];
+		List<ITerm> termList = new LinkedList();
+		for(int i=0; i<indexes.length; i++){
+			if(indexes[i] != -1)
+				terms[indexes[i]] = tuple.getTerm(i);
+		}		
+		for(int j=0; j<indexes.length; j++){
+			if(terms[j] == null)
+				terms[j] = tuple.getTerm(j).getMinValue();
+		}
+		for(int i=0; i<tuple.getArity(); i++)
+			termList.add(terms[i]);
+		return new Tuple(termList);
+	}
 }
+
