525f119af49576bab39ae2889efad9fa89880920
richardpoettler <richardpoettler@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-06-14 07:53:13 +0000

    commit of the first implementation

---
+++ iris/trunk/src/org/deri/iris/basics/Query.java
@@ -0,0 +1,81 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions by 
+ * built-in predicates, default negation (under well-founded semantics), 
+ * function symbols and contexts. 
+ * 
+ * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
+ * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
+ * A-6020 Innsbruck. Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.deri.iris.basics;
+
+import java.util.List;
+
+import org.deri.iris.api.basics.IBody;
+import org.deri.iris.api.basics.ILiteral;
+import org.deri.iris.api.basics.IQuery;
+import org.deri.iris.api.terms.IVariable;
+
+/**
+ * @author richi
+ *
+ */
+public class Query implements IQuery {
+	
+	private IBody body = null;
+	
+	public Query(final IBody body) {
+		this.body = body;
+	}
+
+	public int getQueryLenght() {
+		return body.getBodyLenght();
+	}
+
+	public ILiteral getQueryLiteral(int arg) {
+		return body.getBodyLiteral(arg);
+	}
+
+	public List<ILiteral> getQueryLiterals() {
+		return body.getBodyLiterals();
+	}
+
+	public List<IVariable> getQueryVariables() {
+		return body.getBodyVariables();
+	}
+	
+	public int hashCode() {
+		return body.hashCode();
+	}
+	
+	public boolean equals(final Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (!(o instanceof Query)) {
+			return false;
+		}
+		Query q = (Query) o;
+		return body.equals(q.body);
+	}
+	
+	public String toString() {
+		return body.toString();
+	}
+}
---
+++ iris/trunk/src/org/deri/iris/basics/Rule.java
@@ -0,0 +1,131 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions by 
+ * built-in predicates, default negation (under well-founded semantics), 
+ * function symbols and contexts. 
+ * 
+ * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
+ * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
+ * A-6020 Innsbruck. Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.deri.iris.basics;
+
+import java.util.List;
+
+import org.deri.iris.api.basics.IBody;
+import org.deri.iris.api.basics.IHead;
+import org.deri.iris.api.basics.ILiteral;
+import org.deri.iris.api.basics.IRule;
+import org.deri.iris.api.terms.IVariable;
+
+/**
+ * @author richi
+ *
+ */
+public class Rule implements IRule {
+	
+	private IHead head = null;
+	private IBody body = null;
+	
+	public Rule(final IHead head, final IBody body) {
+		if (body == null) {
+			throw new IllegalArgumentException("The body must not be null");
+		}
+		this.head = head;
+		this.body = body;
+	}
+
+	public boolean isSafe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isRectified() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isCycled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isFact() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isBuiltIn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int getHeadLenght() {
+		return head.getHeadLenght();
+	}
+
+	public ILiteral getHeadLiteral(int arg) {
+		return head.getHeadLiteral(arg);
+	}
+
+	public List<ILiteral> getHeadLiterals() {
+		return head.getHeadLiterals();
+	}
+
+	public List<IVariable> getHeadVariables() {
+		return head.getHeadVariables();
+	}
+
+	public int getBodyLenght() {
+		return body.getBodyLenght();
+	}
+
+	public ILiteral getBodyLiteral(int arg) {
+		return body.getBodyLiteral(arg);
+	}
+
+	public List<ILiteral> getBodyLiterals() {
+		return body.getBodyLiterals();
+	}
+
+	public List<IVariable> getBodyVariables() {
+		return body.getBodyVariables();
+	}
+	
+	public int hashCode() {
+		int result = 37;
+		result = result * 17 + body.hashCode();
+		result = result * 17 + head.hashCode();
+		return result;
+	}
+	
+	public boolean equals(final Object o) {
+		if (o == this) {
+			return true;
+		}
+		if (!(o instanceof Rule)) {
+			return false;
+		}
+		Rule r = (Rule) o;
+		return body.equals(r.body) && head.equals(r.head);
+	}
+	
+	public String toString() {
+		return head + " :- " + body;
+	}
+}
