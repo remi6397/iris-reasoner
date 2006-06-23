50c3530a1d7547fe71cb513e7e34af012765f236
richardpoettler <richardpoettler@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-06-23 07:10:02 +0000

    added some methods (e.g. for the tuple)

--- iris/trunk/api/org/deri/iris/api/factory/IBasicFactory.java
+++ iris/trunk/api/org/deri/iris/api/factory/IBasicFactory.java
@@ -9,29 +9,51 @@ import org.deri.iris.api.basics.ILiteral;
 import org.deri.iris.api.basics.IPredicate;
 import org.deri.iris.api.basics.IQuery;
 import org.deri.iris.api.basics.IRule;
+import org.deri.iris.api.basics.ITuple;
 import org.deri.iris.api.builtins.IBuiltInAtom;
 import org.deri.iris.api.terms.ITerm;
 
 /**
  * @author Darko Anicic, DERI Innsbruck
- * @date   13.01.2006 13:27:34
+ * @date 13.01.2006 13:27:34
  */
 public interface IBasicFactory {
-	
-	public IPredicate createPredicate(String symbol, int arity);
-	
+
+	public IAtom createAtom(IPredicate p);
+
+	public IAtom createAtom(IPredicate p, ITerm... terms);
+
 	public IAtom createAtom(IPredicate p, List<ITerm> terms);
-	
-	public ILiteral createLiteral(boolean isPositive,IAtom atom);
-	
-	public ILiteral createLiteral(boolean isPositive,IBuiltInAtom builtinAtom);
-	
+
+	public IBody createBody(ILiteral... literals);
+
 	public IBody createBody(List<ILiteral> literals);
-	
-	//public IHead createHead(ILiteral literal); 
+
+	public IHead createHead(ILiteral... literals);
+
 	public IHead createHead(List<ILiteral> literals);
-	
-	public IRule createRule(IHead head, IBody body);
-	
+
+	public ILiteral createLiteral(boolean isPositive, IAtom atom);
+
+	public ILiteral createLiteral(boolean isPositive, IBuiltInAtom builtinAtom);
+
+	public ILiteral createLiteral(boolean isPositive, IPredicate predicate);
+
+	public ILiteral createLiteral(boolean isPositive, IPredicate predicate,
+			ITerm... terms);
+
+	public ILiteral createLiteral(boolean isPositive, IPredicate predicate,
+			List<ITerm> terms);
+
+	public IPredicate createPredicate(String symbol, int arity);
+
+	public IQuery createQuery(ILiteral... literals);
+
 	public IQuery createQuery(List<ILiteral> literals);
+
+	public IRule createRule(IHead head, IBody body);
+
+	public ITuple createTuple(ITerm... terms);
+
+	public ITuple createTuple(List<ITerm> terms);
 }
