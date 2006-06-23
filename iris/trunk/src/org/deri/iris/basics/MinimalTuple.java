3ebc6491e5f83aa14ddfa11a096201743f90efbb
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-06-23 14:22:46 +0000

    *** empty log message ***

--- iris/trunk/src/org/deri/iris/basics/MinimalTuple.java
+++ iris/trunk/src/org/deri/iris/basics/MinimalTuple.java
@@ -31,16 +31,14 @@ import java.util.List;
 import org.deri.iris.api.terms.ITerm;
 
 /**
- * @author Darko Anicic, DERI Innsbruck
- * @date   02.06.2006 18:45:38
- */
-
-/**
  * MinimalTuple is a class used for selecting a portion of a tree (relation)
  * that is greater than the MinimalTuple. Instances of this class are used 
  * to distinguish between ordinary tuples of the relation (nodes of the tree)
  * and an artificially made tuple (MinimalTuple) that is created for 
  * selecting purposes.
+ * 
+ * @author Darko Anicic, DERI Innsbruck
+ * @date   02.06.2006 18:45:38
  */
 public class MinimalTuple extends Tuple{
 
