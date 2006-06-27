0340223007af1e72e016872a34f4046abbd91a2b
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-06-27 08:56:10 +0000

    the test improved

--- iris/trunk/test/org/deri/iris/operations/relations/SelectionTest.java
+++ iris/trunk/test/org/deri/iris/operations/relations/SelectionTest.java
@@ -50,41 +50,6 @@ import org.deri.iris.storage.Relation;
  */
 public class SelectionTest extends TestCase {
 
-	/*public SelectionTest() {
-		super();
-	}
-
-	public static void main(String[] args) throws Exception {
-		SelectionTest i = new SelectionTest();
-		i.test();
-	}
-	
-	public void test() throws Exception {
-		IRelation relation0 = new Relation(3);
-		
-		relation0.add(MiscHelper.createTuple("b","b","b"));
-		relation0.add(MiscHelper.createTuple("a","c","c"));
-		relation0.add(MiscHelper.createTuple("b","d","a"));
-		relation0.add(MiscHelper.createTuple("c","a","b"));
-		relation0.add(MiscHelper.createTuple("d","a","b"));
-		
-		Iterator i = relation0.iterator();
-		while(i.hasNext()){
-			System.out.println("relation0: " + ((ITuple)i.next()).toString());
-		}
-
-		ITuple pattern = MiscHelper.createTuple(null, "b", "b");
-		//ITuple pattern = MiscHelper.createTuple("b", "d", "a");
-		
-		ISelection selection = RELATION.createSelectionOperator(
-				relation0, pattern);
-		IRelation result = selection.select();
-		
-		i = result.iterator();
-		while(i.hasNext()){
-			System.out.println("result: " + i.next());
-		}
-	}*/
 	
 	public static Test suite() {
 		return new TestSuite(SelectionTest.class, SelectionTest.class.getSimpleName());
