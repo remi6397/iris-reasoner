c64df01035db1e1b2a26fe79f604f3b3c5620435
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-08-04 15:13:39 +0000

    Adorned Rule + Supplementary Relation for QSQ evaluation

---
+++ iris/trunk/src/org/deri/iris/evaluation/qsq/QSQRule.java
@@ -0,0 +1,80 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions by 
+ * built-in predicates, default negation (under well-founded semantics), 
+ * function symbols and contexts. 
+ * 
+ * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
+ * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
+ * A-6020 Innsbruck. Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.deri.iris.evaluation.qsq;
+
+import java.util.LinkedList;
+import java.util.Set;
+
+import org.deri.iris.evaluation.common.AdornedProgram.AdornedRule;
+
+/**
+ * 
+ * <b>NOTE: At the moment this class only works with rules with one literal
+ * in the head.</b><br/><br/>
+ * 
+ * @author Darko Anicic, DERI Innsbruck
+ * @date   28.07.2006 12:51:07
+ */
+public class QSQRule {
+	
+	private AdornedRule adornedRule;
+	
+	private LinkedList<SupplementaryRelation> supplementaryRelations;
+
+	public QSQRule(AdornedRule ar, LinkedList<SupplementaryRelation> sr) {
+		if ((1 + ar.getBodyLiterals().size()) !=
+				sr.size()) {
+			throw new IllegalArgumentException(
+					"Wrong input parameters! (Set of supplementary " +
+					"relations contains: " + sr.size() + " instead of " +
+					1 + ar.getBodyLiterals().size());
+		}
+		this.adornedRule = ar;
+		this.supplementaryRelations = sr;
+	}
+
+	/**
+	 * @return Returns the rule.
+	 */
+	public AdornedRule getAdornedRule() {
+		return adornedRule;
+	}
+
+	/**
+	 * @param ar The rule to set.
+	 */
+	/*public void setRule(AdornedRule ar) {
+		this.adornedRule = ar;
+	}*/
+
+	/**
+	 * @return Returns the supplementaryRelations.
+	 */
+	public LinkedList<SupplementaryRelation> getSupplementaryRelations() {
+		return supplementaryRelations;
+	}
+	
+}
