6bd199b5d35c164642da38450d99cad9fa10458e
richardpoettler <richardpoettler@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-09-20 08:10:11 +0000

    changed the return type of evaluate to boolean, and added documentation

--- iris/trunk/api/org/deri/iris/api/builtins/IBuiltInAtom.java
+++ iris/trunk/api/org/deri/iris/api/builtins/IBuiltInAtom.java
@@ -1,33 +1,72 @@
 /*
- * MINS (Mins Is Not Silri) A Prolog Egine based on the Silri  
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions by 
+ * built-in predicates, default negation (under well-founded semantics), 
+ * function symbols and contexts. 
  * 
- * Copyright (C) 1999-2005  Juergen Angele and Stefan Decker
- *                          University of Innsbruck, Austria  
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
+ * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
+ * A-6020 Innsbruck. Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
  */
 package org.deri.iris.api.builtins;
 
 import org.deri.iris.api.basics.IAtom;
 
 /**
+ * <p>
+ * Defines a Builtin.
+ * </p>
+ * <p>
+ * $Id: IBuiltInAtom.java,v 1.2 2006-09-20 08:10:11 richardpoettler Exp $
+ * </p>
+ * 
  * @author Darko Anicic, DERI Innsbruck
- * @date   17.03.2006 14:18:27
+ * @author richi
+ * @date 17.03.2006 14:18:27
+ * @version $Revision: 1.2 $
  */
-public abstract interface IBuiltInAtom extends IAtom{
+public abstract interface IBuiltInAtom extends IAtom {
 
-	public void evaluate();
+	/**
+	 * <p>
+	 * Runns the evaluation.
+	 * </p>
+	 * <p>
+	 * If the builting isn't evaluable, <code>false</code> will be returned.
+	 * </p>
+	 * <p>
+	 * If the result of the builtin/evaluation is a boolean (e.g. a equals, or
+	 * less-than builtin) the corresponding value should be returned. If the
+	 * result is more complex, it shoud be stored in an additional term in the
+	 * builtin (e.g. add(x,y,z) should store the sum of x and y in z).
+	 * </p>
+	 * 
+	 * @return <code>true</code> if the evaluation completed successfully and
+	 *         the result (for e.g. a builtin which result is a boolean (e.g.
+	 *         equals)) is <code>true</code>, otherwise <code>false</code>
+	 */
+	public boolean evaluate();
 
+	/**
+	 * Checks whether the builtin is evaluable.
+	 * 
+	 * @return <code>true</code> if the evaluation could be run correctly,
+	 *         otherwise <code>false</code>
+	 */
 	public boolean isEvaluable();
 }
