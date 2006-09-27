e028c7d12a47a11c720264b2fe218352f47acb54
richardpoettler <richardpoettler@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-09-27 12:32:18 +0000

    Corrected the type to IIntegerTerm

--- iris/trunk/api/org/deri/iris/api/terms/concrete/IIntegerTerm.java
+++ iris/trunk/api/org/deri/iris/api/terms/concrete/IIntegerTerm.java
@@ -1,11 +1,10 @@
 package org.deri.iris.api.terms.concrete;
 
 import org.deri.iris.api.terms.INumericTerm;
-import org.deri.iris.terms.concrete.IntegerTerm;
 
 /**
  * @author Darko Anicic, DERI Innsbruck
  * @date   06.01.2006 17:18:34
  */
-public interface IIntegerTerm extends INumericTerm<IntegerTerm, Integer> {
+public interface IIntegerTerm extends INumericTerm<IIntegerTerm, Integer> {
 }
