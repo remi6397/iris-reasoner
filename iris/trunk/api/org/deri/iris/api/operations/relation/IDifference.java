16b7325b772e8b6ada0acde5e93c0a87bf4017b0
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-10-05 09:46:11 +0000

    The interface changed not to take the input parameters (they will be taken from the factory)

--- iris/trunk/api/org/deri/iris/api/operations/relation/IDifference.java
+++ iris/trunk/api/org/deri/iris/api/operations/relation/IDifference.java
@@ -1,21 +1,27 @@
 /*
- * MINS (Mins Is Not Silri) A Prolog Egine based on the Silri  
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions by 
+ * built-in predicates, default negation (under well-founded semantics), 
+ * function symbols and contexts. 
  * 
- * Copyright (C) 1999-2005  Juergen Angele and Stefan Decker
- *                          University of Innsbruck, Austria  
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
+ * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
+ * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
+ * A-6020 Innsbruck. Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
  */
 package org.deri.iris.api.operations.relation;
 
@@ -30,9 +36,9 @@ import org.deri.iris.api.storage.IRelation;
  * @author Darko Anicic
  * @date  11.04.2006 @time  15:25:17
  *
- * @version $Revision: 1.2 $ $Date: 2006-07-27 12:42:26 $
+ * @version $Revision: 1.3 $ $Date: 2006-10-05 09:46:11 $
  */
 public interface IDifference {
 
-	public IRelation difference(final IRelation arg0, final  IRelation arg1);
+	public IRelation difference();
 }
