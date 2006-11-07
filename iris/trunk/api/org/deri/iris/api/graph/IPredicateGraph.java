c86fab05d48b8d12d143c8e9344207ecf3289357
richardpoettler <richardpoettler@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-11-07 13:29:56 +0000

    updated the predicate graph to determine the order of the predicates

--- iris/trunk/api/org/deri/iris/api/graph/IPredicateGraph.java
+++ iris/trunk/api/org/deri/iris/api/graph/IPredicateGraph.java
@@ -25,48 +25,112 @@
  */
 package org.deri.iris.api.graph;
 
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.Set;
 
+import org.deri.iris.api.basics.IPredicate;
 import org.deri.iris.api.basics.IRule;
 
 /**
- * @author richi
+ * <p>
+ * A graph to determine the dependencies of rules and predicates to each other.
+ * </p>
+ * <p>
+ * $Id$
+ * </p>
  * 
+ * @author richi
+ * @version $Revision$
+ * @date $Date$
  */
 public interface IPredicateGraph {
 	/**
-	 * Adds a rule to the specified graph
+	 * Adds a rule to the graph.
 	 * 
 	 * @param rule
 	 *            the rule to add
+	 * @throws NullPointerException
+	 *             if the rule is {@code null}
 	 */
 	public abstract void addRule(final IRule rule);
 
 	/**
-	 * Returnes whether the graph is cycled
+	 * Determines whether the rules are recursive.
 	 * 
-	 * @return true if it is cycled, otherwise false.
+	 * @return {@code true} if they are recursive, otherwise {@code false}
 	 */
 	public abstract boolean detectCycles();
 
 	/**
-	 * Determines the vertexes contained in the cycle
+	 * Determines the vertexes contained in the cycle.
 	 * 
 	 * @return a Set containing all the vertexes
 	 */
 	public abstract Set findVertexesForCycle();
 
 	/**
-	 * Determines the edges contained in the cycle
+	 * Determines the edges contained in the cycle.
 	 * 
 	 * @return a Set containing all the vertexes
 	 */
 	public abstract Set findEdgesForCycle();
-	
+
 	/**
-	 * Returns the number of negative Literals in the cycle
+	 * Returns the number of negative Literals in the cycle.
 	 * 
 	 * @return the number of negative literals
 	 */
 	public abstract int countNegativesForCycle();
+
+	/**
+	 * Adds a collection of rules to the graph.
+	 * 
+	 * @param r
+	 *            the collection of rules
+	 * @throws NullPointerException
+	 *             if the collections is or contains {@code null}
+	 */
+	public abstract void addRule(final Collection<IRule> r);
+
+	/**
+	 * Returns a set of predicates the given one depends on.
+	 * 
+	 * @param p
+	 *            predicate for which to check for dependencies
+	 * @return the predicates the predicate depends on
+	 * @throws NullPointerException
+	 *             if the predicate is {@code null}
+	 */
+	public abstract Set<IPredicate> getDepends(final IPredicate p);
+
+	/**
+	 * <p>
+	 * Returns a compareator which compares two predicates depending on their
+	 * dependencies of their rules.
+	 * </p>
+	 * <p>
+	 * If one of the compared predicate isn't in the graph, or there isn't a
+	 * path from one predicate to the other {@code 0} will be returned. If the
+	 * first predicate depends on the second one, the first one will be
+	 * determined to be bigger, and vice versa.
+	 * </p>
+	 * 
+	 * @return the comparator
+	 */
+	public abstract Comparator<IPredicate> getPredicateComparator();
+
+	/**
+	 * <p>
+	 * Returs a comparator which compares two rules depending on their
+	 * dependencies of each other.
+	 * </p>
+	 * <p>
+	 * The rules will compared according to their headpredicates.
+	 * </p>
+	 * 
+	 * @return the comparator
+	 * @see IPredicateGraph#getPredicateComparator()
+	 */
+	public abstract Comparator<IRule> getRuleComparator();
 }
--- iris/trunk/src/org/deri/iris/graph/PredicateGraph.java
+++ iris/trunk/src/org/deri/iris/graph/PredicateGraph.java
@@ -25,6 +25,11 @@
  */
 package org.deri.iris.graph;
 
+// TODO: implement equals, hashCode, toString an clone.
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -32,49 +37,91 @@ import java.util.Set;
 import org._3pq.jgrapht.DirectedGraph;
 import org._3pq.jgrapht.Edge;
 import org._3pq.jgrapht.GraphHelper;
+import org._3pq.jgrapht.alg.ConnectivityInspector;
 import org._3pq.jgrapht.alg.CycleDetector;
 import org._3pq.jgrapht.graph.DirectedMultigraph;
 import org.deri.iris.api.basics.ILiteral;
+import org.deri.iris.api.basics.IPredicate;
 import org.deri.iris.api.basics.IRule;
 import org.deri.iris.api.graph.IPredicateGraph;
 
 /**
- * @author richi
+ * <p>
+ * A graph to determine the dependencies of rules and predicates to each other.
+ * </p>
+ * <p>
+ * $Id$
+ * </p>
  * 
+ * @author richi
+ * @version $Revision$
+ * @date $Date$
  */
 public class PredicateGraph implements IPredicateGraph {
 
-	private DirectedGraph g = new DirectedMultigraph();
+	/** Comparator to order rules according to their dependencies. */
+	private final RuleComparator rc = new RuleComparator();
 
-	private CycleDetector cd = new CycleDetector(g);
+	/** Comparator to order predicates according to their dependencies. */
+	private final PredicateComparator pc = new PredicateComparator();
 
+	/** Graph to represent the dependencies of the predicates. */
+	private final DirectedGraph g = new DirectedMultigraph();
+
+	/** Cycle detector, to determine, whether the rules are recursive. */
+	private final CycleDetector cd = new CycleDetector(g);
+
+	/**
+	 * Connectivity inspector to determine, whether paths between vertices
+	 * exists.
+	 */
+	private final ConnectivityInspector ci = new ConnectivityInspector(g);
+
+	/**
+	 * Constructs an empty graph object.
+	 */
 	PredicateGraph() {
 	}
 
 	public void addRule(final IRule rule) {
 		if (rule == null) {
-			throw new IllegalArgumentException("The rule must not be null");
+			throw new NullPointerException("The rule must not be null");
 		}
 
-		for (ILiteral h : rule.getHeadLiterals()) {
-			g.addVertex(h.getPredicate());
-			for (ILiteral b : rule.getBodyLiterals()) {
-				boolean doubled = false;
-				g.addVertex(b.getPredicate());
-				Edge e = new LabeledDirectedEdge(b.getPredicate(), h
-						.getPredicate(), b.isPositive());
-				List edges = g.getAllEdges(b.getPredicate(), h.getPredicate());
+		for (final ILiteral h : rule.getHeadLiterals()) {
+			final IPredicate hp = h.getPredicate();
+			g.addVertex(hp);
 
-				for (Object edge : edges) {
+			for (final ILiteral l : rule.getBodyLiterals()) {
+				boolean doubled = false;
+				final IPredicate p = l.getPredicate();
+				final Edge e = new LabeledDirectedEdge<Boolean>(p, hp, l
+						.isPositive());
+
+				g.addVertex(p);
+
+				// determining, whether an equal edge is already in the graph
+				// TODO: use the contains(Object) method
+				for (final Object edge : g.getAllEdges(p, hp)) {
 					if (e.equals(edge)) {
 						doubled = true;
 						break;
 					}
 				}
-				if (!doubled) {
+				if (!doubled) { // if there is no such edge, add the new one
 					g.addEdge(e);
 				}
 			}
+		}
+	}
+
+	public void addRule(final Collection<IRule> r) {
+		if ((r == null) || r.contains(null)) {
+			throw new NullPointerException(
+					"The rules must not be, or contain null");
+		}
+		for (final IRule rule : r) {
+			addRule(rule);
 		}
 	}
 
@@ -86,26 +133,11 @@ public class PredicateGraph implements IPredicateGraph {
 		return cd.findCycles();
 	}
 
-	/**
-	 * This method returns a &quot;set&quot; of edges which are involved in the
-	 * cycle.<br/><br/>NOTE: This Set doesn't behave like a &quot;normal
-	 * set&quot; in the meaning, that it is possible to store multible equal
-	 * object in this set. This is a problem of the underlying implementation,
-	 * which is using <code>HashSet</code> and doesn't implement the
-	 * <code>hashCode</code> method for the objects stored in this set. So, to
-	 * check whether a edge is in the set you sould iterate through the set and
-	 * compare the objects using the equals method. This should work, because
-	 * the edges (LabeledDirectedEdge) used in this graph are impelmenting this
-	 * mehtod.
-	 * 
-	 * @see LabeledDirectedEdge
-	 * @return the set of edges
-	 */
 	public Set<Edge> findEdgesForCycle() {
-		Set cycle = findVertexesForCycle();
-		Set<Edge> edges = new HashSet<Edge>();
-		for (Object v : cycle) {
-			List s = GraphHelper.successorListOf(g, v);
+		final Set cycle = findVertexesForCycle();
+		final Set<Edge> edges = new HashSet<Edge>();
+		for (final Object v : cycle) {
+			final List s = GraphHelper.successorListOf(g, v);
 			for (Object p : s) {
 				if (cycle.contains(p)) {
 					edges.add(g.getEdge(v, p));
@@ -118,8 +150,9 @@ public class PredicateGraph implements IPredicateGraph {
 	}
 
 	public int countNegativesForCycle() {
+		// TODO: optimize this
 		int neg = 0;
-		for (Object e : findEdgesForCycle()) {
+		for (final Object e : findEdgesForCycle()) {
 			if (e instanceof LabeledDirectedEdge) {
 				Object l = ((LabeledDirectedEdge) e).getLabel();
 				if (l instanceof Boolean) {
@@ -130,5 +163,109 @@ public class PredicateGraph implements IPredicateGraph {
 			}
 		}
 		return neg;
+	}
+
+	public Comparator<IRule> getRuleComparator() {
+		return rc;
+	}
+
+	public Comparator<IPredicate> getPredicateComparator() {
+		return pc;
+	}
+
+	public Set<IPredicate> getDepends(final IPredicate p) {
+		if (p == null) {
+			throw new NullPointerException("The predicate must not be null");
+		}
+		if (!g.containsVertex(p)) {
+			return Collections.EMPTY_SET;
+		}
+
+		final Set<IPredicate> todo = new HashSet<IPredicate>();
+		todo.add(p);
+		final Set<IPredicate> deps = new HashSet<IPredicate>();
+
+		while (!todo.isEmpty()) {
+			final IPredicate act = todo.iterator().next();
+			todo.remove(act);
+
+			for (final IPredicate depends : (List<IPredicate>) GraphHelper
+					.predecessorListOf(g, act)) {
+				if (!deps.contains(depends)) {
+					todo.add(depends);
+				}
+				deps.add(depends);
+			}
+		}
+
+		return deps;
+	}
+
+	/**
+	 * <p>
+	 * Compares two rules depending on their dependencies of each other.
+	 * </p>
+	 * <p>
+	 * The rules will compared according to their headpredicates.
+	 * </p>
+	 * <p>
+	 * $Id$
+	 * </p>
+	 * 
+	 * @author richi
+	 * @version $Revision$
+	 * @date $Date$
+	 * @see PredicateComparator
+	 */
+	private class RuleComparator implements Comparator<IRule> {
+
+		public int compare(final IRule o1, final IRule o2) {
+			if ((o1 == null) || (o2 == null)) {
+				throw new NullPointerException("None of the rule must be null");
+			}
+			if ((o1.getHeadLenght() != 1) || (o2.getHeadLenght() != 1)) {
+				throw new IllegalArgumentException(
+						"Only rules with a headlength of 1 are allowed.");
+			}
+			return pc.compare(o1.getHeadLiteral(0).getPredicate(), o2
+					.getHeadLiteral(0).getPredicate());
+		}
+	}
+
+	/**
+	 * <p>
+	 * Compares two predicates depending on their dependencies of their rules.
+	 * </p>
+	 * <p>
+	 * If one of the compared predicate isn't in the graph, or there isn't a
+	 * path from one predicate to the other {@code 0} will be returned. If the
+	 * first predicate depends on the second one, the first one will be
+	 * determined to be bigger, and vice versa.
+	 * </p>
+	 * <p>
+	 * $Id$
+	 * </p>
+	 * 
+	 * @author richi
+	 * @version $Revision$
+	 * @date $Date$
+	 */
+	private class PredicateComparator implements Comparator<IPredicate> {
+
+		public int compare(final IPredicate o1, final IPredicate o2) {
+			if ((o1 == null) || (o2 == null)) {
+				throw new NullPointerException(
+						"None of the predicates must be null");
+			}
+
+			// one of the vertices is not in the graph, or there is no
+			// connection of the vertices -> return 0
+			if (!g.containsVertex(o1) || !g.containsVertex(o2)
+					|| !ci.pathExists(o1, o2)) {
+				return 0;
+			}
+			// determine who depends on who
+			return getDepends(o1).contains(o2) ? 1 : -1;
+		}
 	}
 }
