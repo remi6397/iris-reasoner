85e674f4b71a081780fd6ecaa36931c63ab6253c
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-11-29 17:12:44 +0000

    more constructs method have been added

--- iris/trunk/api/org/deri/iris/api/factory/IModelFactory.java
+++ iris/trunk/api/org/deri/iris/api/factory/IModelFactory.java
@@ -3,7 +3,6 @@ package org.deri.iris.api.factory;
 import org.deri.iris.api.basics.ITuple;
 import org.deri.iris.api.evaluation.seminaive.model.IDifference;
 import org.deri.iris.api.evaluation.seminaive.model.IJoin;
-import org.deri.iris.api.evaluation.seminaive.model.INaturalJoin;
 import org.deri.iris.api.evaluation.seminaive.model.IProjection;
 import org.deri.iris.api.evaluation.seminaive.model.IRule;
 import org.deri.iris.api.evaluation.seminaive.model.ISelection;
@@ -14,15 +13,29 @@ import org.deri.iris.operations.relations.JoinCondition;
 public interface IModelFactory {
 
 	public IProjection createProjection(int[] indexes);
+
 	public ISelection createSelection(ITuple pattern);
+
 	public ISelection createSelection(ITuple pattern, int[] indexes);
+
 	public ISelection createSelection(int[] indexes);
+
 	public IJoin createJoin(int[] indexes, JoinCondition condition);
+
 	public IJoin createJoin(int[] i, JoinCondition c, int[] pi);
+
 	public IJoin createJoin(IJoin j, int[] pi);
+
 	public IDifference createDifference();
+
 	public IUnion createUnion();
+
 	public IRule createRule(String relationName, int relationArity);
+
+	public IRule createRule(boolean positive, String relationName,
+			int relationArity);
+
 	public IRule createUnaryRule(String value);
-	public ITree createTree(String relationName);	
+
+	public ITree createTree(String relationName);
 }
