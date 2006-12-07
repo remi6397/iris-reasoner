80c16d5d32be1e0516058e22d017cb86a99d4364
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-12-07 17:19:32 +0000

    Docs created v1. The following methods removed from the interface as they are not used/needed: isFact isBuiltIn

--- iris/trunk/api/org/deri/iris/api/basics/IRule.java
+++ iris/trunk/api/org/deri/iris/api/basics/IRule.java
@@ -2,19 +2,70 @@ package org.deri.iris.api.basics;
 
 
 /**
+ * <p>
+ * Represents a rule in the program. 
+ * A rule has a form:
+ * </p>
+ * <p>
+ * q :- p1, p2,...,pn 
+ * </p>
+ * <p>
+ * where q is a positive literal (the head), 
+ * and p1, p2,...,pn is a conjunction of several literals (the body). 
+ * Only safe rules are supported. A rule is safe if its every variable
+ * occurs in one of its positive, non built-in, atoms of the body.</p>
+ * <p>
+ * $Id: IRule.java,v 1.3 2006-12-07 17:19:32 darko Exp $
+ * </p>
  * @author Darko Anicic, DERI Innsbruck
  * @date   14.11.2005 17:21:21
  */
 
 public interface IRule extends IHead, IBody{
 	
+	/**
+	 * <p>
+	 * Checks the safeness of a rule.
+	 * </p>
+	 * 
+	 * @return	True if the rule is safe; false otherwise.
+	 */
 	public boolean isSafe();
 	
+	/**
+	 * <p>
+	 * A rule is rectified if its head has the same form as 
+	 * heads of the other rules from the program, 
+	 * e.g. p(X1,…,Xk) for variables X1,…,Xk.
+	 * </p>
+	 * <p> 
+	 * For a given pair of rules:
+	 * </p>
+	 * <ul>
+	 * <li> p(a, X, Y) :- r(X, Y)</li>
+	 * <li> p(X, Y, X) :- r(Y, X)</li>
+	 * </ul>
+	 * <p>
+	 * after the rectification we get the following rules:
+	 * </p>
+	 * <ul>
+	 * <li> p(U, V, W) :- r(V, W), U=’a’.</li>
+	 * <li> p(U, V, W) :- r(V, U), W=U.</li>
+	 * </ul>
+	 * <p>
+	 * where both rules have heads of the same form.
+	 * </p>
+	 * 
+	 * @return	True if the rule is rectified; otherwise false.
+	 */
 	public boolean isRectified();
 	
-	public boolean isCycled();
-	
-	public boolean isFact();
-	
-	public boolean isBuiltIn();
+	/**
+	 * <p>
+	 * Checks whether the entire rule is recursive.
+	 * </p>
+	 * 
+	 * @return	True if the rule is recursive; otherwise false.
+	 */
+	public boolean isRecursive();
 }
