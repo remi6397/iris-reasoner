d14e026403eef3d7344b3f57e830df1f7e09bcfd
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-12-11 15:03:14 +0000

    Docs created v1.

--- iris/trunk/api/org/deri/iris/api/basics/ITuple.java
+++ iris/trunk/api/org/deri/iris/api/basics/ITuple.java
@@ -33,74 +33,131 @@ import org.deri.iris.api.terms.ITerm;
 import org.deri.iris.api.terms.IVariable;
 
 /**
-* @author Darko Anicic, DERI Innsbruck
-* @date   07.12.2005 08:45:24
-*/
+ * <p>
+ * Represents a tuple. A tuple is a list of terms which represents a record in a
+ * relation.
+ * </p>
+ * <p>
+ * $Id: ITuple.java,v 1.8 2006-12-11 15:03:14 darko Exp $
+ * </p>
+ * 
+ * @author Darko Anicic, DERI Innsbruck
+ * @date 07.12.2005 08:45:24
+ */
 
-public interface ITuple extends Comparable<ITuple>{
+public interface ITuple extends Comparable<ITuple> {
 
-	public int getArity();
-	
-	public ITerm getTerm(int arg);
-	
-	public List<ITerm> getTerms();
-	
-	public boolean setTerm(int index, ITerm term);
-	
 	/**
-	 * Inserts all of the elements in the specified collection into this 
-	 * list (starting from 0th position). The new elements 
-	 * will appear in this list in the order that they are returned by 
-	 * the specified collection's iterator. This method is thread-save.
+	 * <p>
+	 * Returns the arity of the predicate.
+	 * </p>
+	 * 
+	 * @return The arity of the predicate.
+	 */
+	public int getArity();
+
+	/**
+	 * <p>
+	 * Returns the term at the specified position in this tuple.
+	 * </p>
+	 * 
+	 * @param index
+	 *            Index of tuple to return.
+	 * @return The term at the specified position in this tuple.
+	 */
+	public ITerm getTerm(int index);
+
+	/**
+	 * <p>
+	 * Returns all terms of this tuple preserving the order of terms in the
+	 * tuple.
+	 * </p>
+	 * 
+	 * @return Returns all terms of this tuple.
+	 */
+	public List<ITerm> getTerms();
+
+	/**
+	 * <p>
+	 * Replaces the term at the specified position in this tuple with the
+	 * specified term.
+	 * </p>
+	 * 
+	 * @param index
+	 *            Index of element to replace.
+	 * @param term
+	 *            Term to be stored at the specified position.
+	 * @return The term which was previously placed at the specified position.
+	 */
+	public ITerm setTerm(int index, ITerm term);
+
+	/**
+	 * Inserts all of the elements in the specified collection into this list
+	 * (starting from 0th position). The new elements will appear in this list
+	 * in the order that they are returned by the specified collection's
+	 * iterator. This method is thread-save.
 	 * 
 	 * @param terms
-	 * 				- elements to be inserted into this list.
-	 * @return true 
-	 * 				if this list changed as a result of the call.
+	 *            Elements to be inserted into this list.
+	 * @return <code>true</code> if this list has been changed as a result of
+	 *         the call; <code>false</code> otherwise.
 	 */
 	public boolean setTerms(List<ITerm> terms);
-	
+
 	/**
-	 * Inserts all of the elements in the specified collection into this 
-	 * list at the specified position. Shifts 
-	 * the element currently at that position (if any) and any subsequent 
-	 * elements to the right.
-	 * Replaces the element currently at that position (if any) and any 
-	 * subsequent elements to the right. It does not increases their indices. 
-	 * Thus an IndexOutOfBoundsException exception will be thrown if size of
-	 * a term list exceeds the number of available positions, which is: 
-	 * 'tuple arity' - index.  
-	 * The new elements will appear in this list in the order that they are 
-	 * returned by the specified collection's iterator. This method is 
+	 * Inserts all of the elements in the specified collection into this list at
+	 * the specified position. Shifts the element currently at that position (if
+	 * any) and any subsequent elements to the right. Replaces the element
+	 * currently at that position (if any) and any subsequent elements to the
+	 * right. It does not increases their indices. Thus an
+	 * IndexOutOfBoundsException exception will be thrown if size of a term list
+	 * exceeds the number of available positions, which is: 'tuple arity' -
+	 * index. The new elements will appear in this list in the order that they
+	 * are returned by the specified collection's iterator. This method is
 	 * thread-save.
 	 * 
 	 * @param index
-	 * 				- index at which to insert first element from 
-	 * 				the specified collection.
+	 *            Index at which to insert first element from the specified
+	 *            collection.
 	 * @param terms
-	 * 				- elements to be inserted into this list.
-	 * @return true 
-	 * 				if this list changed as a result of the call.
+	 *            Elements to be inserted into this list.
+	 * @return <code>true</code> if this list has been changed as a result of
+	 *         the call; <code>false</code> otherwise.
 	 */
 	public boolean setTerms(int index, List<ITerm> terms);
-	
-	public boolean isGround();
-	
-	public Set<IVariable> getVariables();
-	
+
 	/**
-	 * Tuples t0 and t1 are duplicates if they have identical terms  
-	 * for each sort index. Used in operations on relations 
-	 * (e.g. for tuple sorting � IndexComparator) to increase the 
-	 * efficiency of the operations.
+	 * <p>
+	 * Checks whether this tuple contains only ground terms.
+	 * </p>
+	 * 
+	 * @return <code>true</code> if all terms of this tuple are grounded;
+	 *         <code>false</code> otherwise.
+	 */
+	public boolean isGround();
+
+	/**
+	 * <p>
+	 * Returns all distinct variables from this tupple.
+	 * </p>
+	 * 
+	 * @return All distinct variables from this tupple.
+	 */
+	public Set<IVariable> getVariables();
+
+	/**
+	 * Tuples t0 and t1 are duplicates if they have identical terms for each
+	 * index. Used with implementations of relational operations (e.g. for
+	 * sorting tuples with IndexComparator) to increase the efficiency of the
+	 * operations.
 	 * 
 	 * @param duplicate
-	 * 					a tuple that will be stored in a list of 
-	 * 					duplicates for the entire tuple.  
+	 *            A tuple that will be stored in a list of duplicates for the
+	 *            entire tuple.
 	 * @deprecated a duplicate shouldn't be stored in the tuple
 	 */
 	public void setDuplicate(ITuple duplicate);
-	
+
 	/**
 	 * @deprecated a duplicate shouldn't be stored in the tuple
 	 */
