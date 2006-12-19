220e82b156a311a84ee85aea684f3742368070d3
darko <darko@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2006-12-19 18:16:24 +0000

    New algebra data model made (to replace the model from Paco)

---
+++ iris/trunk/src/org/deri/iris/evaluation/algebra/Rule2Relation.java
@@ -0,0 +1,451 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions by 
+ * built-in predicates, default negation (under well-founded semantics), 
+ * function symbols and contexts. 
+ * 
+ * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
+ * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
+ * A-6020 Innsbruck. Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.deri.iris.evaluation.algebra;
+
+import static org.deri.iris.factory.Factory.ALGEBRA;
+import static org.deri.iris.factory.Factory.BASIC;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+import org.deri.iris.api.basics.IAtom;
+import org.deri.iris.api.basics.ILiteral;
+import org.deri.iris.api.basics.IPredicate;
+import org.deri.iris.api.basics.IQuery;
+import org.deri.iris.api.basics.IRule;
+import org.deri.iris.api.basics.ITuple;
+import org.deri.iris.api.builtins.IBuiltInAtom;
+import org.deri.iris.api.evaluation.algebra.IComponent;
+import org.deri.iris.api.evaluation.algebra.IConstantDescriptor;
+import org.deri.iris.api.evaluation.algebra.IJoinDescriptor;
+import org.deri.iris.api.evaluation.algebra.IProjectionDescriptor;
+import org.deri.iris.api.evaluation.algebra.IRelationDescriptor;
+import org.deri.iris.api.evaluation.algebra.ISelectionDescriptor;
+import org.deri.iris.api.evaluation.algebra.IUnionDescriptor;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.api.terms.IVariable;
+import org.deri.iris.basics.seminaive.NonEqualityTerm;
+import org.deri.iris.builtins.EqualBuiltin;
+import org.deri.iris.builtins.UnEqualBuiltin;
+import org.deri.iris.factory.Factory;
+import org.deri.iris.operations.relations.JoinCondition;
+import org.deri.iris.operations.relations.Projection;
+
+/**
+ * <p>
+ * Algorithm 3.1 (Chapter 3. Logic as a data model, Principles of Database and
+ * Knowledge - Base Systems, Ullman):
+ * </p>
+ * <p>
+ * Computing the relation for a rule body, using relational algebra operations.
+ * </p>
+ * 
+ * <p>Remark: Constructed terms are not considered in the current
+ * Rule2Relation transformation!</p>
+ * 
+ * @author Darko Anicic, DERI Innsbruck
+ * @date Dec 12, 2006
+ * 
+ */
+public class Rule2Relation {
+
+	/** prefix for the variables */
+	private static final String VAR_PREFIX = "?X_";
+
+	/** counter for an arbitrarly chosen variable */
+	private static int VAR_COUNTER = 0;
+
+	private Map<ILiteral, List<IVariable>> m = null;
+
+	private Set<IVariable> oVars = null;
+
+	/**
+	 * <p>
+	 * Transform a set of rules into relational algebra expressions
+	 * </p>
+	 * 
+	 * @param rules
+	 *            A set of rules to be translated
+	 * @return A map of IDB predicates with corresponding relational algebra
+	 *        	  expressions
+	 */
+	public Map<IPredicate, IComponent> translateRules(final Set<IRule> rules) {
+
+		Map<IPredicate, IComponent> results = new Hashtable<IPredicate, IComponent>();		
+		
+		Map<IPredicate, IUnionDescriptor> unionMap = new Hashtable<IPredicate, IUnionDescriptor>();		
+		
+		IComponent c = null;
+		
+		for (IRule r : rules) {
+			m = new TreeMap<ILiteral, List<IVariable>>();
+			oVars = new HashSet<IVariable>();
+			IPredicate p = r.getHeadLiteral(0).getPredicate();
+			IAtom a = null;
+			ISelectionDescriptor s = null;
+			
+			IComponent body = translateBody(r.getBodyLiterals());
+			results.put(p, body);
+			/**
+			 * <p>
+			 * SELECTION
+			 * </p>
+			 * <p>
+			 * EVAL-RULE(r, R1,...,Rn) = SELECTION_F(E) F conjunction of
+			 * built-in subgoals appearing in the rule body.
+			 * </p>
+			 */
+			for (ILiteral l : r.getBodyLiterals()) {
+				a = l.getAtom();
+				if (a instanceof IBuiltInAtom) {
+					if (a.getTuple().getTerm(0) instanceof IVariable &&
+							a.getTuple().getTerm(1) instanceof IVariable) {
+						
+						if (a instanceof EqualBuiltin) {
+							s = ALGEBRA.createSelectionDescriptor(a.getTuple());
+						}
+						if (a instanceof UnEqualBuiltin) {
+							// TODO: Remove/don't use: NonEqualityTerm
+							List<ITerm> terms = new ArrayList<ITerm>(a.getTuple()
+									.getArity());
+							for (ITerm t : a.getTuple().getTerms()) {
+								terms.add(new NonEqualityTerm(t));
+							}
+							s = ALGEBRA.createSelectionDescriptor(BASIC
+									.createTuple(terms));
+						}
+						s.addChild(results.get(p));
+						s.addVariables(body.getVariables());
+						results.put(p, s);
+					}
+				}
+			}
+			/**
+			 * <p>
+			 * PROJECTION
+			 * </p>
+			 * <p>
+			 * Overall projection considering the rule head.
+			 * </p>
+			 */
+			IProjectionDescriptor pr = null;
+			c = results.get(p);
+			
+			if (!Arrays.equals(r.getHeadVariables().toArray(), c.getVariables().toArray())) {
+				int[] pInds = getProjectionIndexes(r.getHeadVariables(), c.getVariables());
+				pr = ALGEBRA.createProjectionDescriptor(pInds);
+				pr.addChild(results.get(p));
+				pr.addVariables(filterProjectionVariables(pInds, c.getVariables()));
+				results.put(p, pr);
+			}
+			/**
+			 * <p>
+			 * UNION
+			 * </p>
+			 * <p>
+			 * Overall union considering the rules with identical heads.
+			 * </p>
+			 * <p>
+			 * Check whether there is a rule with the same head predicate; if
+			 * so, the bodies must be united.
+			 * </p>
+			 * <p>
+			 * The relations should be union-compatible: (a) Same number of
+			 * fields. (b) Corresponding fields have the same type.
+			 * <p>
+			 */
+			IUnionDescriptor u = null;
+			c = unionMap.get(p);
+			IComponent comp = results.get(p);
+			if(c != null){
+				c.addChild(comp);
+				unionMap.put(p, (IUnionDescriptor)c);
+			}else{
+				u = ALGEBRA.createUnionDescriptor();
+				u.addChild(comp);
+				u.addVariables(comp.getVariables());
+				unionMap.put(p, u);
+			}
+		}
+		results.clear();
+		for(IPredicate pred : unionMap.keySet()){
+			c = unionMap.get(pred);
+			if(c.getChildren().size() > 1){
+				results.put(pred, c);
+			}else{
+				results.put(pred, c.getChildren().get(0));
+			}
+		}
+		return results;
+	}
+
+	public Map<IPredicate, IComponent> translateQueries(final Set<IQuery> queries) {
+		Map<IPredicate, IComponent> results = new Hashtable<IPredicate, IComponent>();		
+		
+		for(IQuery q : queries){
+			results.put(
+					q.getQueryLiteral(0).getPredicate(), 
+					translateBody(q.getQueryLiterals()));
+		}
+		return results;
+	}
+	
+	/**
+	 * <p>
+	 * INPUT:
+	 * </p>
+	 * <p>
+	 * Body of rule r = S1,...,Sn with variables X1,...,Xm;
+	 * </p>
+	 * <p>
+	 * Si = pi(Ai1,...,Aiki) where pi <--> Ri & Ai = variable or constant.
+	 * </p>
+	 * 
+	 * @param lits
+	 *            A body of a rule to be translated.
+	 * @return An expression of relational algebra.
+	 */
+	private IComponent translateBody(final List<ILiteral> lits) {
+		IJoinDescriptor j = ALGEBRA.createJoinDescriptor(JoinCondition.EQUALS);
+
+		/** a. Ordinary literal (subgoal) */
+		for (ILiteral l : lits) {
+			if (!(l.getAtom() instanceof IBuiltInAtom)) {
+				IComponent c = translateOrdinaryLiteral(l);
+				if(c != null){
+					j.addChild(c);
+					j.addVariables(c.getVariables());
+				}
+			}
+		}
+		/**
+		 * b. Built-in literal (subgoal): Note: Built-in subgoals need to be
+		 * handled after the ordinary subgoals
+		 */
+		for (ILiteral l : lits) {
+			if (l.getAtom() instanceof IBuiltInAtom) {
+				IComponent c = translateBuiltInAtom(l.getAtom());
+				if(c != null){
+					j.addChild(c);
+					j.addVariables(c.getVariables());
+				}
+			}
+		}
+		if(j.getChildren().size() == 1){
+			return j.getChildren().get(0);
+		} else {
+			return j;
+		}
+	}
+
+	private IComponent translateOrdinaryLiteral(final ILiteral l) {
+		ITuple pattern = null;
+		int[] indexes = new int[l.getPredicate().getArity()];
+		int[] projectInds = Projection.getInitIndexes(l.getPredicate().getArity());
+		List<ITerm> ts = new ArrayList<ITerm>(l.getPredicate().getArity());
+		List<ITerm> terms = l.getAtom().getTuple().getTerms();
+		List<IVariable> vars = new ArrayList<IVariable>(l.getPredicate()
+				.getArity());
+		int i = 0, j = 0;
+		ITerm t1 = null;
+		boolean selectionNeeded = false;
+		boolean projectionNeeded = false;
+
+		for (ITerm t : terms) {
+			if (t.isGround()) {
+				ts.add(t);
+				vars.add(createFreshVariable());
+				selectionNeeded = true;
+				projectionNeeded = true;
+			} else {
+				ts.add(null);
+				projectInds[i] = j++;
+				// TODO: Constructed terms are not considered in Rule2Relation
+				// transformation
+				vars.add((IVariable) t);
+				for (int k = ++i; k < terms.size(); k++) {
+					t1 = terms.get(k);
+					if (t.equals(t1)) {
+						indexes[i] = i;
+						selectionNeeded = true;
+					} else {
+						indexes[i] = 0;
+					}
+				}
+			}
+		}
+		pattern = BASIC.createTuple(ts);
+		IRelationDescriptor le = ALGEBRA.createRelationDescriptor(l
+				.isPositive(), l.getPredicate());
+
+		le.addVariables(vars);
+		m.put(l, vars);
+		oVars.addAll(vars);
+		ISelectionDescriptor s = null;
+		if (selectionNeeded) {
+			s = ALGEBRA.createSelectionDescriptor(pattern, indexes);
+			s.addChild(le);
+			s.addVariables(le.getVariables());
+			if (!projectionNeeded)
+				return s;
+		}
+		if (selectionNeeded && projectionNeeded) {
+			IProjectionDescriptor p = ALGEBRA
+					.createProjectionDescriptor(projectInds);
+			p.addChild(s);
+			p.addVariables(filterProjectionVariables(projectInds, s.getVariables()));
+			return p;
+		}
+		return le;
+	}
+
+	private IComponent translateBuiltInAtom(final IAtom a) {
+		ITerm t1 = a.getTuple().getTerm(0);
+		ITerm t2 = a.getTuple().getTerm(1);
+		int[] projectInds = new int[a.getPredicate().getArity()];
+
+		// ?X = ?Y or ?X != ?Y
+		if (t1 instanceof IVariable && t2 instanceof IVariable) {
+			if ((!oVars.contains(t1)) || (!oVars.contains(t2))) {
+
+				// ?X = ?Y --> Dx = PROJECTIONj(Ri) (Si(...,?Y, ...))
+				Iterator<ILiteral> i = m.keySet().iterator();
+				ILiteral l = null;
+				List<IVariable> v = null;
+
+				while (i.hasNext()) {
+					l = i.next();
+					v = m.get(l);
+					if (v.contains(t2)) {
+						ITerm tt = t1;
+						t1 = t2; // t1 = ?X
+						t2 = tt; // t2 = ?Y
+						break;
+					}
+					if (v.contains(t1) || v.contains(t2)) {
+						break;
+					}
+				}
+				List<ITerm> terms = l.getTuple().getTerms();
+				List<IVariable> vars = new ArrayList<IVariable>(terms.size());
+				List<IVariable> distinctVars = new ArrayList<IVariable>();
+				/**
+				 * (b) If Y appears as the jth argument of ordinary subgoal Si,
+				 * let Dx be PROJECTIONj(Ri)
+				 */
+				ITerm t = null;
+				int j;
+				for (j = 0; j < terms.size(); j++) {
+					t = terms.get(j);
+					if (t.equals(t1)) {
+						vars.add((IVariable) t2);
+						distinctVars.add((IVariable) t2);
+						break;
+					} else {
+						vars.add(createFreshVariable());
+					}
+				}
+				for (int k = j + 1; k < terms.size(); k++) {
+					vars.add(createFreshVariable());
+				}
+				for (int k = 0; k < terms.size(); k++) {
+					if (k != j) {
+						projectInds[k] = -1;
+					} else {
+						projectInds[k] = 0;
+					}
+				}
+				IRelationDescriptor r = ALGEBRA.createRelationDescriptor(true,
+						l.getPredicate());
+
+				r.addVariables(vars);
+				IProjectionDescriptor p = ALGEBRA
+						.createProjectionDescriptor(projectInds);
+
+				p.addChild(r);
+				p.addVariables(distinctVars);
+				return p;
+			}
+		}else {
+			// ?X = 'a'
+			if (t2 instanceof IVariable) {
+				ITerm tt = t1;
+				t1 = t2; // t1 = ?X
+				t2 = tt; // t2 = 'a'
+			}
+			IConstantDescriptor c = ALGEBRA.createConstantDescriptor(t2,
+					(IVariable) t1);
+			return c;
+		}
+		return null;
+	}
+
+	private IVariable createFreshVariable() {
+		IVariable v = Factory.TERM.createVariable(VAR_PREFIX + VAR_COUNTER++);
+		return v;
+	}
+
+	private int[] getProjectionIndexes(final List<IVariable> vars0,
+			final List<IVariable> vars1) {
+		int j = 0, k=0;
+		int[] inds = new int[vars1.size()];
+
+		for (j = 0; j < vars1.size(); j++) {
+			inds[j] = -1;
+		}
+		IVariable v0 = null;
+		IVariable v1 = null;
+		for (int i=0; i<vars0.size(); i++) {
+			v0 = vars0.get(i);
+			for (j=0; j<vars1.size(); j++) {
+				v1 = vars1.get(j);
+				if (v0.equals(v1)) {
+					inds[j] = k++;
+					break;
+				}
+			}
+		}
+		return inds;
+	}
+
+	private List<IVariable> filterProjectionVariables(final int[] pInds,
+			final List<IVariable> vars) {
+		List<IVariable> v = new ArrayList<IVariable>(pInds.length);
+		for (int i = 0; i < pInds.length; i++) {
+			if (pInds[i] != -1) {
+				v.add(vars.get(i));
+			}
+		}
+		return v;
+	}
+}
