2e2bb44797e7c02b5851c5bf00c1fd5bf06838e4
holgerlausen <holgerlausen@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2007-07-18 06:13:58 +0000

    initial iris web site skeleton

---
+++ iris/trunk/web/pages/download.php
@@ -0,0 +1,6 @@
+<h1>Download</h1>
+
+<p>IRIS releases is available at <a href="http://sourceforge.net/project/platformdownload.php?group_id=167309">sourceforge</a>.</p>
+
+<h2>Daily Build</h2>
+<p>We also offer <a href="snapshot">daily builds</a>.</p>
---
+++ iris/trunk/web/pages/home.php
@@ -0,0 +1,7 @@
+<h1>IRIS Reasoner</h1>
+      <p>IRIS - Integrated Rule Inference System is an extensible reasoning engine for expressive rule-based languages 
+(WSML Core/Flight/Rule), as well as description logic based languages (WSML-DL). Currently IRIS supports reasoning 
+with WSML Core/Flight.</p>
+
+<h2>News</h2>
+<p><tt>2007-07-13</tt> We have created an initial draft of our web site and introduced a <a href="snapshot">daily build</a> system</p>
