616cf1790ee7f55b69ad6e0b47a7032db4c17aa5
holgerlausen <holgerlausen@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2007-07-23 13:27:14 +0000

    some minor extensions

--- iris/trunk/web/pages/download.php
+++ iris/trunk/web/pages/download.php
@@ -1,6 +1,31 @@
+<style type="text/css">ul{margin-top:-20px;}</style>
+
+
 <h1>Download</h1>
 
 <p>IRIS releases is available at <a href="http://sourceforge.net/project/platformdownload.php?group_id=167309">sourceforge</a>.</p>
 
-<h2>Daily Build</h2>
-<p>We also offer <a href="snapshot">daily builds</a>.</p>
+<h2>Release History</h2>
+<p><b>v0.3</b> 2007-06-15</p> 
+<ul>
+	<li>enabled multible programs per vm</li>
+</ul>
+
+<p><b>v0.2</b> 2007-02-09</p> 
+<ul>
+	<li>Support for all data types supported by WSML</li>
+	<li>A numbr of built-ins implemented</li>
+	<li>added the IndexingOnTheFlyRelation and corresponding implementations</li>
+	<li>Added the IRelationFactory interface</li>
+	<li>modified the IBuiltinInterface</li>
+	<li>reimplemented the Complementor class</li>
+	<li>added the possibility to make custom builitns</li>
+	<li>bugfixes</li>
+</ul>
+
+<p><b>v0.1</b> 2007-02-08</p> 
+<ul><li>initial release</li></ul>
+
+<h2>Daily Snapshot</h2>
+<p>We also offer a <a href="snapshot">daily snapshot</a>.
+</p>
