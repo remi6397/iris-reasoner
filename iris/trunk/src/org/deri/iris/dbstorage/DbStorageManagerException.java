640e337629bad9a69b53c2708df9ed1f59c1dd30
fefacca <fefacca@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2007-07-29 10:45:49 +0000

    db integration package

---
+++ iris/trunk/src/org/deri/iris/dbstorage/DbStorageManagerException.java
@@ -0,0 +1,24 @@
+package org.deri.iris.dbstorage;
+
+public class DbStorageManagerException extends Exception {
+
+	public DbStorageManagerException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbStorageManagerException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbStorageManagerException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DbStorageManagerException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
