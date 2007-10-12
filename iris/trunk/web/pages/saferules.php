6802b90f444b6de404acf7b5d5289461ca716893
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2007-10-12 13:04:49 +0000

    Updated the safe-rules description for the new handling of arithmetic predicates.

--- iris/trunk/web/pages/saferules.php
+++ iris/trunk/web/pages/saferules.php
@@ -3,9 +3,8 @@
 <p>
 	The algorithm for detecting unsafe rules is now located in a dedicated
 	class: RuleValidator. This algorithm is taken from Principles of Database and
-	Knowledgebase Systems, Ullman, page 105.
+	Knowledgebase Systems, Ullman, page 105:
 </p>
-
 
 <p>
 	A rule is considered safe if all variables are limited.
@@ -18,21 +17,20 @@
 		<li>It appears in a positive equality with a constant, e.g. ?X = 'a'</li>
 		<li>It appears in a positive equality with another variable known to be limited, e.g. ?X = ?Y, ?Y = 'a'</li>
 	</ul>
-
 <p>
 	However, rule validation in IRIS can be parameterised to allow the relaxation
 	of two aspects of this algorithm, specifically:
 	<ul>
 		<li>
 			variables that ONLY appear in a negated ordinary predicate (and nowhere
-			else) can still make for a safe rule, as such a rule can be re-written to
+			else) can still make for a safe rule, because such a rule can be re-written to
 			move the negated sub-goal to a separate rule, see the example in Ullman,
 			page 129-130
 		</li>
 		<li>
-			variables that only appear as a target of a ternary predicate can be
-			considered limited if the operands of the predicate are themselves limited,
-			e.g. ?X + ?Y = ?Z, where ?Z can be considered limited if both ?X and ?Y are.
+			Furthermore, variables that appear in arithmetic predicates can also be considered limited if
+			all the other variables are known to be limited, e.g.
+			?X + ?Y = ?Z, ?X = 3, ?Z = 4, implies that ?Y is also limited
 		</li>
 	</ul>
 </p>
