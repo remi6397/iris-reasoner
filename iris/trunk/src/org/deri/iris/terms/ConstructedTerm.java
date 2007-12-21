ce331964e01b3d1523533016a68ce7d186520b0d
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2007-12-21 10:46:55 +0000

    Improved toString() and tidied up lots of stuff.

--- iris/trunk/src/org/deri/iris/terms/ConstructedTerm.java
+++ iris/trunk/src/org/deri/iris/terms/ConstructedTerm.java
@@ -52,18 +52,20 @@ public class ConstructedTerm implements IConstructedTerm {
 	 * A constructed term consist of a list of terms, where these terms 
 	 * can be constructed or non-constructed ones in a general case.
 	 */
-	private List<ITerm> terms = new ArrayList<ITerm>();
+	private final List<ITerm> terms = new ArrayList<ITerm>();
 
-	private String symbol = "";
+	/** The function symbol itself. */
+	private final String symbol;
 	
-	
+	/**
+	 * Constructor (for factory method).
+	 * @param symbol The function symbol
+	 * @param terms The terms for this function symbols expression.
+	 */
 	ConstructedTerm(final String symbol, final Collection<ITerm> terms) {
-		if (symbol == null) {
-			throw new NullPointerException("The symbol must not be nulL");
-		}
-		if (terms == null) {
-			throw new NullPointerException("The terms must not be null");
-		}
+		assert symbol != null : "The symbol must not be null";
+		assert terms != null : "The terms must not be null";
+		
 		this.symbol = symbol;
 		this.terms.addAll(terms);
 	}
@@ -85,42 +87,40 @@ public class ConstructedTerm implements IConstructedTerm {
 	}
 
 	public boolean isGround() {
-		for(Object term : this.terms){
-			if(term instanceof ConstructedTerm){
-				if(!isGround((IConstructedTerm)term)) return false;
-			}else{
-				if (!( (ITerm) term).isGround()) return false;
-			}
+		for(ITerm term : terms){
+			if (! term.isGround() )
+				return false;
 				
 		}
 		return true;
 	}
 	
-	private boolean isGround(IConstructedTerm t) {
-		List<ITerm> termList = (List<ITerm>)t.getValue();
-		for(ITerm term : termList){
-			if(term instanceof ConstructedTerm){
-				if(!isGround((IConstructedTerm)term)) return false;
-			}else{
-				if (!term.isGround()) return false;
-			}
-		}
-		return true;
-	}
-	
 	public int hashCode() {
-		int result = 17;
-		result = result * 37 + symbol.hashCode();
+		int result = symbol.hashCode();
 		for (Object t : terms) {
 			result = result * 37 + t.hashCode();
 		}
 		return result;
 	}
 
-	public String toString() {
-		return symbol + terms.toString();
+	public String toString()
+	{
+		StringBuilder result = new StringBuilder();
+		
+		result.append( symbol ).append( '(' );
+		
+		for( int i = 0; i < terms.size(); ++i )
+		{
+			if( i > 0 )
+				result.append( ',' );
+			result.append( terms.get( i ) );
+		}
+		
+		result.append( ')' );
+		
+		return result.toString();
 	}
-
+	
 	public boolean equals(final Object o) {
 		if (!(o instanceof ConstructedTerm)) {
 			return false;
@@ -131,51 +131,41 @@ public class ConstructedTerm implements IConstructedTerm {
 	}
 
 	public int compareTo(final ITerm oo) {
-		if (oo == null) {
-			return 1;
-		}
 		
+		assert oo instanceof IConstructedTerm : "Invalid argument type for ConstructedTerm.compare()";
+	
 		final IConstructedTerm o = (IConstructedTerm) oo;
 		
 		int result = symbol.compareTo(o.getFunctionSymbol());
-		if (result != 0) {
+		if (result != 0)
 			return result;
-		}
 		
-		int min = Math.min(terms.size(), ((List<ITerm>)o.getValue()).size());
+		List<ITerm> oTerms = (List<ITerm>) o.getValue();
+		
+		int min = Math.min( terms.size(), oTerms.size() );
 		
 		for (int iCounter = 0; iCounter < min; iCounter++) {
-			result = terms.get(iCounter).compareTo(o.getParameter(iCounter));
+			result = terms.get(iCounter).compareTo(oTerms.get(iCounter));
 			if (result != 0)
 				return result;
 		}
-		return terms.size() - ((List<ITerm>)o.getValue()).size();
+		return terms.size() - o.getValue().size();
 	}
 
 	public List<ITerm> getValue() {
-		return this.terms;
+		return terms;
 	}
 
 	public Set<IVariable> getVariables() {
 		Set<IVariable> variables = new HashSet<IVariable>();
-		for(Object term : this.terms){
+		for(ITerm term : terms){
 			if(term instanceof IVariable)
 				variables.add((IVariable)term);
-			if(term instanceof IConstructedTerm)
-				variables.addAll(getVariables((IConstructedTerm)term));
-				
-		}
-		return variables;
-	}
-	
-	private Set<IVariable> getVariables(IConstructedTerm t) {
-		Set<IVariable> variables = new HashSet<IVariable>();
-		List<ITerm> termList = (List<ITerm>)t.getValue();
-		for(ITerm term : termList){
-			if(term instanceof IVariable)
-				variables.add((IVariable)term);
-			if(term instanceof IConstructedTerm)
-				variables.addAll(getVariables((IConstructedTerm)term));
+			else if(term instanceof IConstructedTerm)
+			{
+				IConstructedTerm childTerm = (IConstructedTerm) term;
+				variables.addAll(childTerm.getVariables() );
+			}
 		}
 		return variables;
 	}
