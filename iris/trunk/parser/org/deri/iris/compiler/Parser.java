41feaceba544c858bd9e71efb9a9d5a04fdbc74c
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2008-01-31 12:30:28 +0000

    Removed old pre-function symbols code.

--- iris/trunk/parser/org/deri/iris/compiler/Parser.java
+++ iris/trunk/parser/org/deri/iris/compiler/Parser.java
@@ -25,82 +25,58 @@
  */
 package org.deri.iris.compiler;
 
-import static org.deri.iris.factory.Factory.PROGRAM;
-
 import java.io.IOException;
 import java.io.PushbackReader;
 import java.io.Reader;
 import java.io.StringReader;
-
-import org.deri.iris.api.IProgram;
+import java.util.List;
+import java.util.Map;
+import org.deri.iris.api.basics.IPredicate;
+import org.deri.iris.api.basics.IQuery;
+import org.deri.iris.api.basics.IRule;
+import org.deri.iris.builtins.BuiltinRegister;
+import org.deri.iris.new_stuff.storage.IRelation;
 import org.deri.iris.parser.lexer.Lexer;
 import org.deri.iris.parser.lexer.LexerException;
 import org.deri.iris.parser.parser.ParserException;
 
 /**
- * <p>
- * Parser to parse datalog programs.
- * </p>
- * <p>
- * $Id: Parser.java,v 1.9 2007-09-27 14:49:44 bazbishop237 Exp $
- * </p>
- * @author Francisco Garcia
- * @author Richard Pöttler, richard dot poettler at deri dot org
- * @version $Revision: 1.9 $
+ * Parses a datalog program in human readable form in to an IRIS object model.
  */
-public class Parser {
-     
-	/**
-	 * Parses a datalog program.
-	 * @param prog the program to parse
-	 * @return a newly created program represented the parsed one.
-	 * @throws org.deri.iris.compiler.ParserException 
-	 * @throws ParserException if something went wrong while
-	 * parsing
-	 * @throws IllegalArgumentException if the program is <code>null</code>
-	 */
-	public static IProgram parse(final String prog) throws org.deri.iris.compiler.ParserException {
-		if (prog == null) {
-			throw new IllegalArgumentException("The string to parse must not be null");
-		}
-		return parse(prog, null);
+public class Parser
+{
+	public Parser( BuiltinRegister builtinRegister )
+	{
+		mBuiltinRegister = builtinRegister;
 	}
-
+	
 	/**
 	 * Parses a datalog program.
-	 * @param r the reader from where to read the program
+	 * @param program The program to parse. This must not be null.
 	 * @return a newly created program represented the parsed one.
-	 * @throws org.deri.iris.compiler.ParserException 
-	 * @throws ParserException if something went wrong while
-	 * parsing
-	 * @throws IllegalArgumentException if the reader is <code>null</code>
+	 * @throws org.deri.iris.compiler.ParserException
 	 */
-	public static IProgram parse(final Reader r) throws org.deri.iris.compiler.ParserException {
-		if (r == null) {
+	public void parse( String program ) throws org.deri.iris.compiler.ParserException
+	{
+		if ( program == null )
 			throw new IllegalArgumentException("The reader must not be null");
-		}
-		return parse(r, null);
+		
+		parse(new StringReader(program) );
 	}
-
-	/**
-	 * Parses a datalog string. The parsed object will be add to the
-	 * submitted program.
-	 * @param prog the string to parse
-	 * @param p the program where to add the objects. Might be
-	 * <code>null</code>
-	 * @return the input program, or a newly created one (if the input
-	 * program was <code>null</code>) containing all the objects of the
-	 * parsed one.
-	 * @throws org.deri.iris.compiler.ParserException 
-	 * @throws ParserException if something went wrong while
-	 * parsing
-	 * @throws IllegalArgumentException if the string is <code>null</code>
-	 */
-	public static IProgram parse(final String prog, final IProgram p) throws org.deri.iris.compiler.ParserException {
-		if (prog == null) {
-			throw new IllegalArgumentException("The string to parse must not be null");
-		}
-		return parse(new StringReader(prog), p);
+	
+	public List<IRule> getRules()
+	{
+		return mTreeWalker.getRuleBase();
+	}
+	
+	public Map<IPredicate,IRelation> getFacts()
+	{
+		return mTreeWalker.getFacts();
+	}
+	
+	public List<IQuery> getQueries()
+	{
+		return mTreeWalker.getQueries();
 	}
 
 	/**
@@ -116,16 +92,18 @@ public class Parser {
 	 * parsing
 	 * @throws IllegalArgumentException if the reader is <code>null</code>
 	 */
-	public static IProgram parse(final Reader r, final IProgram p) throws org.deri.iris.compiler.ParserException{
-		if (r == null) {
+	public void parse(final Reader r) throws org.deri.iris.compiler.ParserException
+	{
+		if (r == null)
 			throw new IllegalArgumentException("The reader must not be null");
-		}
-		final IProgram prog = (p == null) ? PROGRAM.createProgram() : p;
+		
+		mTreeWalker = new TreeWalker( mBuiltinRegister );
+
 		try
 		{
 			org.deri.iris.parser.parser.Parser parser = new org.deri.iris.parser.parser.Parser( new Lexer( new PushbackReader(r, 1024) ) );
 
-			parser.parse().apply( new TreeWalker( prog ) );
+			parser.parse().apply( mTreeWalker );
 		}
 		catch (ParserException e)
 		{
@@ -147,7 +125,8 @@ public class Parser {
 			// Some errors (such as wrong number of arguments for a type) manifest themselves as IllegalArgumentExceptions.
 			throw new org.deri.iris.compiler.ParserException( e.getMessage() );
 		}
-		
-		return prog;
 	}
+	
+	private TreeWalker mTreeWalker;
+	private final BuiltinRegister mBuiltinRegister;
 }
