6d2b54e073e6063eda56aae30d71c558324476ed
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2008-02-28 11:49:39 +0000

    Updates for latest features and team members.

--- iris/trunk/web/pages/team.php
+++ iris/trunk/web/pages/team.php
@@ -5,7 +5,6 @@
 <a href="http://www.deri.at/about/team/details/barry-bishop/">Barry Bishop</a><br/>
 <a href="http://www.deri.at/about/team/details/richard-poettler/">Richard P&ouml;ttler</a><br/>
 <a href="http://www.deri.at/about/team/details/florian-fischer/">Florian Fischer</a><br/>
-<a href="http://www.deri.at/about/team/details/cristina-feier/">Cristina Feier</a><br/>
 <a href="http://www.uwekeller.net/">Uwe Keller</a><br/>
 <a href="http://www.deri.at/about/team/details/nathalie-steinmetz/">Nathalie Steinmetz</a><br/>
 <a href="http://www.holgerlausen.net/">Holger Lausen</a><br/>
@@ -14,6 +13,7 @@
 <dt>Previous members</dt>
 <dd>
 <a href="http://darko.anicic.googlepages.com/">Darko Anicic</a><br/>
+<a href="http://www.deri.at/about/team/details/cristina-feier/">Cristina Feier</a><br/>
 </dd>
 
 
