aabe77475efc9a82da75212e4acaaef49816c78a
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2008-02-28 12:45:11 +0000

    Disabled this test.

--- iris/trunk/app/org/deri/iris/demo/DemoTest.java
+++ iris/trunk/app/org/deri/iris/demo/DemoTest.java
@@ -22,56 +22,56 @@
  * MA  02110-1301, USA.
  */
 package org.deri.iris.demo;
-
-import junit.framework.TestCase;
-import org.deri.iris.KnowledgeBaseFactory;
-
-public class DemoTest extends TestCase
-{
-	public void testSimpleProgram() throws Exception
-	{
-    	String program = 
-		    "p(1)." +
-		    "p(2)." +
-		    
-		    "q(?X) :- p(?X)." +
-		    "?- q(?x).";
-    	
-    	helperExecuteProgram( program );
-	}
-
-	public void testEmptyProgram() throws Exception
-	{
-    	helperExecuteProgram( "" );
-	}
-	
-	public void testProgramWithoutQuery() throws Exception
-	{
-    	String program = 
-		    "p(1)." +
-		    "p(2)." +
-		    
-		    "q(?X) :- p(?X).";
-    
-    	helperExecuteProgram( program );
-	}
-
-	public void testProgramwithTwoQueries() throws Exception
-	{
-    	String program = 
-		    "p(1)." +
-		    "p(2)." +
-		    
-		    "q(?X) :- p(?X)." +
-		    "?- p(?x)." +
-    		"?- q(?x).";
-    	
-    	helperExecuteProgram( program );
-	}
-
-	private void helperExecuteProgram( String program ) throws Exception
-	{
-    	Demo.main( new String[]{ Demo.PROGRAM + "=" + program, Demo.UNSAFE_RULES, Demo.WELL_FOUNDED } );
-    	Demo.execute( program, KnowledgeBaseFactory.getDefaultConfiguration() );
-	}
-}
+//
+//import junit.framework.TestCase;
+//import org.deri.iris.KnowledgeBaseFactory;
+//
+//public class DemoTest extends TestCase
+//{
+//	public void testSimpleProgram() throws Exception
+//	{
+//    	String program = 
+//		    "p(1)." +
+//		    "p(2)." +
+//		    
+//		    "q(?X) :- p(?X)." +
+//		    "?- q(?x).";
+//    	
+//    	helperExecuteProgram( program );
+//	}
+//
+//	public void testEmptyProgram() throws Exception
+//	{
+//    	helperExecuteProgram( "" );
+//	}
+//	
+//	public void testProgramWithoutQuery() throws Exception
+//	{
+//    	String program = 
+//		    "p(1)." +
+//		    "p(2)." +
+//		    
+//		    "q(?X) :- p(?X).";
+//    
+//    	helperExecuteProgram( program );
+//	}
+//
+//	public void testProgramwithTwoQueries() throws Exception
+//	{
+//    	String program = 
+//		    "p(1)." +
+//		    "p(2)." +
+//		    
+//		    "q(?X) :- p(?X)." +
+//		    "?- p(?x)." +
+//    		"?- q(?x).";
+//    	
+//    	helperExecuteProgram( program );
+//	}
+//
+//	private void helperExecuteProgram( String program ) throws Exception
+//	{
+//    	Demo.main( new String[]{ Demo.PROGRAM + "=" + program, Demo.UNSAFE_RULES, Demo.WELL_FOUNDED } );
+//    	Demo.execute( program, KnowledgeBaseFactory.getDefaultConfiguration() );
+//	}
+//}
