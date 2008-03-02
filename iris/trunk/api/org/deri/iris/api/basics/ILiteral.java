95f1849a17afd695e2eb2fcab2adaf93fe43bff4
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2008-03-02 23:13:23 +0000

    Updated to new file-header (STI).

--- iris/trunk/api/org/deri/iris/api/basics/ILiteral.java
+++ iris/trunk/api/org/deri/iris/api/basics/ILiteral.java
@@ -1,12 +1,9 @@
 /*
  * Integrated Rule Inference System (IRIS):
- * An extensible rule inference system for datalog with extensions by 
- * built-in predicates, default negation (under well-founded semantics), 
- * function symbols and contexts. 
+ * An extensible rule inference system for datalog with extensions.
  * 
- * Copyright (C) 2006  Digital Enterprise Research Institute (DERI), 
- * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
- * A-6020 Innsbruck. Austria.
+ * Copyright (C) 2008 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -22,7 +19,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
  * MA  02110-1301, USA.
- * dation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 package org.deri.iris.api.basics;
 
