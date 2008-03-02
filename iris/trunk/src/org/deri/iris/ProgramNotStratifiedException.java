19aadae690aaa7ac32753b33997514a8d6eb514d
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2008-03-02 23:12:01 +0000

    Updated to new file-header (STI).

--- iris/trunk/src/org/deri/iris/ProgramNotStratifiedException.java
+++ iris/trunk/src/org/deri/iris/ProgramNotStratifiedException.java
@@ -1,12 +1,9 @@
 /*
  * Integrated Rule Inference System (IRIS):
- * An extensible rule inference system for datalog with extensions by 
- * built-in predicates, default negation (under well-founded semantics), 
- * function symbols and contexts. 
+ * An extensible rule inference system for datalog with extensions.
  * 
- * Copyright (C) 2007  Digital Enterprise Research Institute (DERI), 
- * Leopold-Franzens-Universitaet Innsbruck, Technikerstrasse 21a, 
- * A-6020 Innsbruck. Austria.
+ * Copyright (C) 2008 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
