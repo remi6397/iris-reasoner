6f505a41981831f8d1e7df50dff6e360df716cd5
adrian_marte <adrian_marte@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2009-06-22 13:58:07 +0000

    Built-in with 2 arguments is now separate class. Formated code and edited JavaDoc. Removed unnecessary assertions.

--- iris/trunk/src/org/deri/iris/builtins/string/StringStartsWithBuiltin.java
+++ iris/trunk/src/org/deri/iris/builtins/string/StringStartsWithBuiltin.java
@@ -28,95 +28,69 @@ import org.deri.iris.api.basics.IPredicate;
 import org.deri.iris.api.terms.IStringTerm;
 import org.deri.iris.api.terms.ITerm;
 import org.deri.iris.builtins.BooleanBuiltin;
+import org.deri.iris.terms.StringTerm;
 
 /**
- * Represents a string starts-with operation as described in
- * http://www.w3.org/TR/xpath-functions/#func-starts-with.
+ * Represents the RIF built-in func:starts-with as described in
+ * http://www.w3.org/TR/xpath-functions/#func-starts-with. At the moment only
+ * Unicode code point collation
+ * (http://www.w3.org/2005/xpath-functions/collation/codepoint) is supported.
  */
 public class StringStartsWithBuiltin extends BooleanBuiltin {
 
-	private static final IPredicate PREDICATE1 = BASIC.createPredicate(
-			"STRING_STARTSWITH2", 2);
-	private static final IPredicate PREDICATE2 = BASIC.createPredicate(
-			"STRING_STARTSWITH3", 3);
+	private static final IPredicate PREDICATE = BASIC.createPredicate(
+			"STRING_STARTS_WITH3", 3);
 
 	/**
 	 * Constructor.
 	 * 
-	 * @param haystack
-	 *            The term representing the haystack, i.e. the string being
-	 *            searched for the occurrence of the needle.
-	 * @param needle
-	 *            The term representing the needle, i.e. the string to be
-	 *            searched for in the haystack.
-	 * @throws IllegalArgumentException
-	 *             if one of the terms is {@code null}
+	 * @param terms The terms, where the term at the first position is the
+	 *            <code>haystack</code>, the term at the second position is the
+	 *            <code>needle</code> and the term at the third position is the
+	 *            collation to be used. The <code>haystack</code> is the string
+	 *            being searched for the occurrence of the <code>needle</code>.
+	 *            The <code>needle</code> is the string to be searched for in
+	 *            the <code>haystack</code>.
+	 * @throws IllegalArgumentException if one of the terms is {@code null}
 	 */
-	public StringStartsWithBuiltin(final ITerm haystack, final ITerm needle) {
-		super(PREDICATE1, new ITerm[] { haystack, needle });
-	}
-
-	/**
-	 * Constructor.
-	 * 
-	 * @param haystack
-	 *            The term representing the haystack, i.e. the string being
-	 *            searched for the occurrence of the needle.
-	 * @param needle
-	 *            The term representing the needle, i.e. the string to be
-	 *            searched for in the haystack.
-	 * @param collation
-	 *            The collation to be used. Currently only "Minimal match" is
-	 *            supported.
-	 * @throws IllegalArgumentException
-	 *             if one of the terms is {@code null}
-	 */
-	public StringStartsWithBuiltin(final ITerm haystack, final ITerm needle,
-			final ITerm collation) {
-		super(PREDICATE2, new ITerm[] { haystack, needle, collation });
+	public StringStartsWithBuiltin(ITerm... terms) {
+		super(PREDICATE, terms);
 	}
 
 	@Override
 	protected boolean computeResult(ITerm[] terms) {
-		assert terms.length >= 2 && terms.length <= 3;
-
 		String haystack = null;
 		String needle = null;
+		String collation = null;
 
-		// The default collation.
-		String collation = "Minimal Match";
-
-		if (terms[0] instanceof IStringTerm && terms[1] instanceof IStringTerm) {
+		if (terms[0] instanceof IStringTerm && terms[1] instanceof StringTerm
+				&& terms[2] instanceof StringTerm) {
 			haystack = ((IStringTerm) terms[0]).getValue();
 			needle = ((IStringTerm) terms[1]).getValue();
+			collation = ((IStringTerm) terms[2]).getValue();
 		} else {
 			return false;
 		}
 
+		return startsWith(haystack, needle, collation);
+	}
+
+	public static boolean startsWith(String haystack, String needle,
+			String collation) {
+		String defaultCollation = "http://www.w3.org/2005/xpath-functions/collation/codepoint";
+
+		// Only "Unicode code point collation" is supported at the moment.
+		if (collation != null && !collation.equalsIgnoreCase(defaultCollation)) {
+			throw new IllegalArgumentException("Unsupported collation");
+		}
+
 		// If the value of haystack is the zero-length string, the function
 		// returns false.
-		if (haystack.length() == 0 && needle.length() > 0) {
+		if (haystack.length() == 0) {
 			return false;
 		}
 
-		if (terms.length > 2) {
-			if (terms[2] instanceof IStringTerm) {
-				collation = ((IStringTerm) terms[2]).getValue();
-
-				// Only "Minimal Match" is supported at the moment.
-				if (!collation.equalsIgnoreCase("Minimal Match")) {
-					throw new IllegalArgumentException("Unsupported collation");
-				}
-			} else {
-				return false;
-			}
-		}
-
 		return haystack.startsWith(needle);
-	}
-
-	public int maxUnknownVariables() {
-		return 0;
 	}
 
 }
