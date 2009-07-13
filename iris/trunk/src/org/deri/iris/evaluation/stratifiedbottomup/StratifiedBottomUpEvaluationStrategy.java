397936716c0588f5793f5b09b08a7b98660c41d6
adrian_marte <adrian_marte@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2009-07-13 11:51:17 +0000

    Added support for rules with head equality.

--- iris/trunk/src/org/deri/iris/evaluation/stratifiedbottomup/StratifiedBottomUpEvaluationStrategy.java
+++ iris/trunk/src/org/deri/iris/evaluation/stratifiedbottomup/StratifiedBottomUpEvaluationStrategy.java
@@ -24,6 +24,7 @@ package org.deri.iris.evaluation.stratifiedbottomup;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import org.deri.iris.Configuration;
 import org.deri.iris.EvaluationException;
 import org.deri.iris.api.basics.IQuery;
@@ -36,73 +37,85 @@ import org.deri.iris.rules.compiler.ICompiledRule;
 import org.deri.iris.rules.compiler.RuleCompiler;
 import org.deri.iris.rules.safety.AugmentingRuleSafetyProcessor;
 import org.deri.iris.storage.IRelation;
+import org.deri.iris.utils.equivalence.IEquivalentTerms;
 
 /**
  * A strategy that uses bottom up evaluation on a stratified rule set.
  */
-public class StratifiedBottomUpEvaluationStrategy implements IEvaluationStrategy
-{
-	StratifiedBottomUpEvaluationStrategy( IFacts facts, List<IRule> rules, IRuleEvaluatorFactory ruleEvaluatorFactory, Configuration configuration ) throws EvaluationException
-	{
+public class StratifiedBottomUpEvaluationStrategy implements
+		IEvaluationStrategy {
+	StratifiedBottomUpEvaluationStrategy(IFacts facts, List<IRule> rules,
+			IRuleEvaluatorFactory ruleEvaluatorFactory,
+			Configuration configuration) throws EvaluationException {
 		mConfiguration = configuration;
-		
 		mRuleEvaluatorFactory = ruleEvaluatorFactory;
-
 		mFacts = facts;
+		mEquivalentTerms = mConfiguration.equivalentTermsFactory.createEquivalentTerms();
 
-		if( mConfiguration.ruleSafetyProcessor instanceof AugmentingRuleSafetyProcessor )
-			facts = new FiniteUniverseFacts( facts, rules );
-		
-		EvaluationUtilities utils = new EvaluationUtilities( mConfiguration );
-		
+		List<IRule> allRules = mConfiguration.ruleHeadEqualityPreProcessor
+				.process(rules, facts);
+
+		if (mConfiguration.ruleSafetyProcessor instanceof AugmentingRuleSafetyProcessor)
+			facts = new FiniteUniverseFacts(facts, allRules);
+
+		EvaluationUtilities utils = new EvaluationUtilities(mConfiguration);
+
 		// Rule safety processing
-		List<IRule> safeRules = utils.applyRuleSafetyProcessor( rules );
+		List<IRule> safeRules = utils.applyRuleSafetyProcessor(allRules);
 
 		// Stratify
-		List<List<IRule>> stratifiedRules = utils.stratify( safeRules );
-		
-		RuleCompiler rc = new RuleCompiler( facts, mConfiguration );
-		
-		for( List<IRule> stratum : stratifiedRules )
-		{
+		List<List<IRule>> stratifiedRules = utils.stratify(safeRules);
+
+		RuleCompiler rc = new RuleCompiler(facts, mEquivalentTerms,
+				mConfiguration);
+
+		for (List<IRule> stratum : stratifiedRules) {
 			// Re-order stratum
-			List<IRule> reorderedRules = utils.reOrderRules( stratum );
-			
+			List<IRule> reorderedRules = utils.reOrderRules(stratum);
+
 			// Rule optimisation
-			List<IRule> optimisedRules = utils.applyRuleOptimisers( reorderedRules );
+			List<IRule> optimisedRules = utils
+					.applyRuleOptimisers(reorderedRules);
 			
 			List<ICompiledRule> compiledRules = new ArrayList<ICompiledRule>();
-			
-			for( IRule rule : optimisedRules )
-				compiledRules.add( rc.compile( rule ) );
-			
+
+			for (IRule rule : optimisedRules) {
+				compiledRules.add(rc.compile(rule));
+			}
+
 			IRuleEvaluator eval = mRuleEvaluatorFactory.createEvaluator();
-			eval.evaluateRules( compiledRules, facts, configuration );
+			eval.evaluateRules(compiledRules, facts, configuration);
 		}
-		
 	}
 
-	public IRelation evaluateQuery( IQuery query, List<IVariable> outputVariables ) throws EvaluationException
-	{
-		if( query == null )
-			throw new IllegalArgumentException( "StratifiedBottomUpEvaluationStrategy.evaluateQuery() - query must not be null." ); 
+	public IRelation evaluateQuery(IQuery query, List<IVariable> outputVariables)
+			throws EvaluationException {
+		if (query == null)
+			throw new IllegalArgumentException(
+					"StratifiedBottomUpEvaluationStrategy.evaluateQuery() - query must not be null.");
 
-		if( outputVariables == null )
-			throw new IllegalArgumentException( "StratifiedBottomUpEvaluationStrategy.evaluateQuery() - outputVariables must not be null." ); 
+		if (outputVariables == null)
+			throw new IllegalArgumentException(
+					"StratifiedBottomUpEvaluationStrategy.evaluateQuery() - outputVariables must not be null.");
 
-		RuleCompiler compiler = new RuleCompiler( mFacts, mConfiguration );
-		
-		ICompiledRule compiledQuery = compiler.compile( query );
-		
+		RuleCompiler compiler = new RuleCompiler(mFacts, mEquivalentTerms,
+				mConfiguration);
+
+		ICompiledRule compiledQuery = compiler.compile(query);
+
 		IRelation result = compiledQuery.evaluate();
-		
+
 		outputVariables.clear();
-		outputVariables.addAll( compiledQuery.getVariablesBindings() );
+		outputVariables.addAll(compiledQuery.getVariablesBindings());
 
 		return result;
 	}
 
+	protected IEquivalentTerms mEquivalentTerms;
+
 	protected final Configuration mConfiguration;
+
 	protected final IFacts mFacts;
+
 	protected final IRuleEvaluatorFactory mRuleEvaluatorFactory;
 }
