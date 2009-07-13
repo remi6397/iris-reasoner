9eed7c14c8544496da88b58ab713a8c75a168af9
adrian_marte <adrian_marte@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2009-07-13 11:55:07 +0000

    Added method to create tuple of constants.

--- iris/trunk/test/org/deri/iris/rules/compiler/Helper.java
+++ iris/trunk/test/org/deri/iris/rules/compiler/Helper.java
@@ -34,43 +34,66 @@ import org.deri.iris.api.terms.ITerm;
 /**
  * Helper methods for the tests in this package.
  */
-public class Helper
-{
-	public static ITerm createTerm( Object t )
-	{
-		if( t instanceof Integer )
-			return CONCRETE.createInteger( (Integer) t );
-		else if( t instanceof String )
-			return TERM.createVariable( (String) t );
-		else if( t instanceof ITerm )
+public class Helper {
+	public static ITerm createTerm(Object t) {
+		if (t instanceof Integer)
+			return CONCRETE.createInteger((Integer) t);
+		else if (t instanceof String)
+			return TERM.createVariable((String) t);
+		else if (t instanceof ITerm)
 			return (ITerm) t;
-		
-		throw new RuntimeException( "Unsupported term type in Helper.createTerm()" );
+
+		throw new RuntimeException(
+				"Unsupported term type in Helper.createTerm()");
 	}
 
-	public static List<ITerm> createTerms( Object ... termObjects )
-	{
-    	List<ITerm> terms = new ArrayList<ITerm>();
-        
-    	for( Object o : termObjects )
-    		terms.add( createTerm( o ) );
-    	
-    	return terms;
+	public static List<ITerm> createTerms(Object... termObjects) {
+		List<ITerm> terms = new ArrayList<ITerm>();
+
+		for (Object o : termObjects)
+			terms.add(createTerm(o));
+
+		return terms;
 	}
 
-	public static ITuple createTuple( Object ... termObjects )
-    {
-    	return BASIC.createTuple( createTerms( termObjects ) );
-    }
+	public static ITuple createTuple(Object... termObjects) {
+		return BASIC.createTuple(createTerms(termObjects));
+	}
 
-	public static ITerm createConstructedTerm( String symbol, Object ... termObjects )
-    {
-    	return TERM.createConstruct( symbol, createTerms( termObjects ) );
-    }
-	
-	public static ILiteral createLiteral( boolean positive, String predicate, Object ... termObjects )
-	{
-		ITuple tuple = createTuple( termObjects );
-		return BASIC.createLiteral( positive, BASIC.createPredicate( predicate, tuple.size() ), tuple );
+	public static ITerm createConstant(Object t) {
+		if (t instanceof Integer)
+			return CONCRETE.createInteger((Integer) t);
+		else if (t instanceof String)
+			return TERM.createString((String) t);
+		else if (t instanceof ITerm)
+			return (ITerm) t;
+
+		throw new RuntimeException(
+				"Unsupported term type in Helper.createTerm()");
+	}
+
+	public static List<ITerm> createConstants(Object... termObjects) {
+		List<ITerm> terms = new ArrayList<ITerm>();
+
+		for (Object o : termObjects)
+			terms.add(createConstant(o));
+
+		return terms;
+	}
+
+	public static ITuple createConstantTuple(Object... termObjects) {
+		return BASIC.createTuple(createConstants(termObjects));
+	}
+
+	public static ITerm createConstructedTerm(String symbol,
+			Object... termObjects) {
+		return TERM.createConstruct(symbol, createTerms(termObjects));
+	}
+
+	public static ILiteral createLiteral(boolean positive, String predicate,
+			Object... termObjects) {
+		ITuple tuple = createTuple(termObjects);
+		return BASIC.createLiteral(positive, BASIC.createPredicate(predicate,
+				tuple.size()), tuple);
 	}
 }
