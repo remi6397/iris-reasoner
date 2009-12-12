ad358ad7fae1b1024ed36d3c5a31f51b982e1155
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2009-12-12 10:37:44 +0000

    Made some fixes for proper support of XML schema data-type definition (with slight modifications for the OWL 2 working group).

--- iris/trunk/test/org/deri/iris/terms/concrete/DoubleTest.java
+++ iris/trunk/test/org/deri/iris/terms/concrete/DoubleTest.java
@@ -22,66 +22,87 @@
  */
 package org.deri.iris.terms.concrete;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
-import org.deri.iris.ObjectTests;
-import org.deri.iris.TermTests;
-
-/**
- * <p>
- * Tests the functionality of the <code>DoubleTerm</code>.
- * </p>
- * <p>
- * $Id$
- * </p>
- * 
- * @author Richard Pöttler, richard dot poettler at deri dot org
- * @version $Revision$
- */
 public class DoubleTest extends TestCase {
 
-	private final static double BASIC = 0.1d;
-
-	private final static double MORE = 0.2d;
-
-	private final static double MORE1 = 0.3d;
-
-	public static Test suite() {
-		return new TestSuite(DoubleTest.class, DoubleTest.class.getSimpleName());
+	public void testConstruct() {
+		DoubleTerm dt = new DoubleTerm(0.123);
+		assertEquals( 0.123, dt.getValue() );
 	}
-
-	public void testBasic() {
-		DoubleTerm basic = new DoubleTerm(BASIC);
-
-		assertEquals("object not initialized correctly", BASIC, basic
-				.getValue());
-	}
-
+	
 	public void testEquals() {
-		ObjectTests.runTestEquals(new DoubleTerm(BASIC), new DoubleTerm(BASIC),
-				new DoubleTerm(MORE));
+		checkEqual( +0.0, +0.0 );
+		checkEqual( -0.0, -0.0 );
+		checkEqual( 1.1, 1.1 );
+		checkEqual( -1.1, -1.1 );
+		checkEqual( Double.NaN, Double.NaN );
+		checkEqual( Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY );
+		checkEqual( Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY );
+		checkEqual( Double.MAX_VALUE, Double.MAX_VALUE );
+		checkEqual( Double.MIN_VALUE, Double.MIN_VALUE );
+		checkEqual( Double.MIN_NORMAL, Double.MIN_NORMAL );
+	}
+	
+	public void testNotEquals() {
+		checkNotEqual( +0.0, -0.0 );
+		checkNotEqual( +0.0, -0.0 );
+		checkNotEqual( 0.0, Double.MIN_NORMAL );
+		checkNotEqual( 0.0, -Double.MIN_NORMAL );
+		checkNotEqual( Double.NaN, 0.0 );
+		checkNotEqual( Double.POSITIVE_INFINITY, 0.0 );
+		checkNotEqual( Double.NEGATIVE_INFINITY, 0.0 );
+		checkNotEqual( Double.NaN, 0.0 );
 	}
 
 	public void testCompare() {
-		ObjectTests.runTestCompareTo(new DoubleTerm(BASIC),
-				new DoubleTerm(BASIC), new DoubleTerm(MORE), new DoubleTerm(
-						MORE1));
-	}
-
-	public void testEqualsPositiveNegativeZero() {
-		ObjectTests.runTestCompareTo(new DoubleTerm(+0.0),
-				new DoubleTerm(-0.0), new DoubleTerm(0.000000001), new DoubleTerm(
-								0.000000002));
+		checkLess( 0.0, Double.MIN_VALUE );
+		checkLess( -Double.MIN_VALUE, 0.0 );
+		checkLess( Double.NEGATIVE_INFINITY, 0.0 );
+		checkLess( 0.0, Double.POSITIVE_INFINITY );
 	}
 
 	public void testHashCode() {
-		ObjectTests
-				.runTestHashCode(new DoubleTerm(BASIC), new DoubleTerm(BASIC));
+		checkSameHashCode( 1.234, 1.234 );
+		checkSameHashCode( 0.0, 0.0 );
+		checkSameHashCode( Double.NaN, Double.NaN );
 	}
 
-	public void testGetMinValue() {
-		TermTests.runTestGetMinValue(new DoubleTerm(Double.MIN_VALUE + 0.0001));
+	private void checkSameHashCode( double f1, double f2 ) {
+		DoubleTerm ft1 = new DoubleTerm( f1 );
+		DoubleTerm ft2 = new DoubleTerm( f2 );
+		
+		assertEquals( ft1.hashCode(), ft2.hashCode() );
+	}
+
+	private void checkEqual( double f1, double f2 ) {
+		DoubleTerm ft1 = new DoubleTerm( f1 );
+		DoubleTerm ft2 = new DoubleTerm( f2 );
+		
+		assertEquals( ft1, ft2 );
+		assertEquals( ft2, ft1 );
+		assertTrue( ft1.compareTo( ft2 ) == 0 );
+		assertTrue( ft2.compareTo( ft1 ) == 0 );
+	}
+
+	private void checkNotEqual( double f1, double f2 ) {
+		DoubleTerm ft1 = new DoubleTerm( f1 );
+		DoubleTerm ft2 = new DoubleTerm( f2 );
+		
+		assertFalse( ft1.equals( ft2 ) );
+		assertFalse( ft2.equals( ft1 ) );
+		assertTrue( ft1.compareTo( ft2 ) != 0 );
+		assertTrue( ft2.compareTo( ft1 ) != 0 );
+	}
+
+	private void checkLess( double f1, double f2 ) {
+		DoubleTerm ft1 = new DoubleTerm( f1 );
+		DoubleTerm ft2 = new DoubleTerm( f2 );
+		
+		assertTrue( ft1.compareTo( ft2 ) < 0 );
+		assertTrue( ft2.compareTo( ft1 ) > 0 );
+
+		assertFalse( ft1.equals( ft2 ) );
+		assertFalse( ft2.equals( ft1 ) );
 	}
 }
