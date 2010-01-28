3ba9185d7db074749fdef91a005d0662f1813918
matpre <matpre@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2010-01-28 15:49:59 +0000

    Added Tests for Date builtins.

---
+++ iris/trunk/test/org/deri/iris/builtins/date/DateSubtractBuiltinTest.java
@@ -0,0 +1,65 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions.
+ * 
+ * Copyright (C) 2009 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package org.deri.iris.builtins.date;
+
+import org.deri.iris.EvaluationException;
+import org.deri.iris.api.terms.ITerm;
+import org.deri.iris.factory.Factory;
+
+/**
+ */
+public class DateSubtractBuiltinTest extends AbstractDateBuiltinTest {
+	
+	public DateSubtractBuiltinTest(String name) {
+		super(name);
+	}
+
+	public void testBuiltin() throws EvaluationException {
+		ITerm date1 = Factory.CONCRETE.createDate(2011, 5, 27);
+		ITerm date2 = Factory.CONCRETE.createDate(2010, 4,  1);
+		ITerm date3 = Factory.CONCRETE.createDate(2010, 4,  1);
+
+		DateSubtractBuiltin builtin = new DateSubtractBuiltin(date1, date2, X); 	
+		
+		args = Factory.BASIC.createTuple(X, X, X);
+		actual = builtin.evaluate(args);
+	    expected = Factory.BASIC.createTuple(Factory.CONCRETE.createDuration(true, 1, 1, 26, 0, 0, 0, 0));
+   
+	    assertEquals(expected, actual);
+		
+		builtin = new DateSubtractBuiltin(date2, date3, Y); 	
+		args = Factory.BASIC.createTuple(X, X, X);
+		expected = Factory.BASIC.createTuple(Factory.CONCRETE.createDuration(true, 0, 0, 0, 0, 0, 0, 0));
+		actual = builtin.evaluate(args);
+		
+		assertEquals(expected, actual );
+		
+		builtin = new DateSubtractBuiltin(date3, date1, Y); 	
+		args = Factory.BASIC.createTuple(X, X, X);
+		expected = Factory.BASIC.createTuple(Factory.CONCRETE.createDuration(false, 1, 1, 26, 0, 0, 0, 0));
+		actual = builtin.evaluate(args);
+		
+		assertEquals(expected, actual );
+	}
+
+}
