b5d1aa85ce9a280251af0d0afdc3b7b98ac9f9a4
bazbishop237 <bazbishop237@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2010-02-24 21:22:11 +0000

    Tag for release 0.60

---
+++ iris/tags/iris_0_6_0/web/pages/team.php
@@ -0,0 +1,21 @@
+<h1>Team</h1>
+
+<dt>Active members</dt>
+<dd>
+<a href="http://www.deri.at/about/team/details/barry-bishop/">Barry Bishop</a><br/>
+<a href="http://www.deri.at/about/team/details/florian-fischer/">Florian Fischer</a><br/>
+<a href="http://www.uwekeller.net/">Uwe Keller</a><br/>
+<a href="http://www.deri.at/about/team/details/nathalie-steinmetz/">Nathalie Steinmetz</a><br/>
+<a href="http://www.sti-innsbruck.at/about/team/details/christoph-fuchs/">Christoph 'Gigi' Fuchs</a><br/>
+<a href="http://www.sti-innsbruck.at/about/team/details/matthias-pressnig/">Matthias Pressnig</a><br/>
+</dd>
+
+<dt>Previous members</dt>
+<dd>
+<a href="http://darko.anicic.googlepages.com/">Darko Anicic</a><br/>
+<a href="http://www.deri.at/about/team/details/cristina-feier/">Cristina Feier</a><br/>
+<a href="http://www.holgerlausen.net/">Holger Lausen</a><br/>
+<a href="http://www.deri.at/about/team/details/richard-poettler/">Richard P&ouml;ttler</a><br/>
+</dd>
+
+
