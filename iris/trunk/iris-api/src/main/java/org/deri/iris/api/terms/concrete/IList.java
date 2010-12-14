1c4a8ca82d56900e422af4c4669eaa52d1ff9a48
adrian_marte <adrian_marte@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2010-12-14 22:51:31 +0000

    Now also takes into account the equivalent terms when determining the equality of two lists.

--- iris/trunk/iris-api/src/main/java/org/deri/iris/api/terms/concrete/IList.java
+++ iris/trunk/iris-api/src/main/java/org/deri/iris/api/terms/concrete/IList.java
@@ -23,6 +23,7 @@
 package org.deri.iris.api.terms.concrete;
 
 import org.deri.iris.api.terms.IConcreteTerm;
+import org.deri.iris.utils.equivalence.IEquivalentTerms;
 
 public interface IList extends IConcreteTerm, java.util.List<IConcreteTerm> {
 
@@ -30,5 +31,22 @@ public interface IList extends IConcreteTerm, java.util.List<IConcreteTerm> {
 	 * Defines the RIF List URI.
 	 */
 	public static String DATATYPE_URI = "http://www.w3.org/2007/rif#List";
+
+	/**
+	 * Checks if this list equals the specified list taking into account the
+	 * specified equivalent terms to determine the equality of the items of the
+	 * lists.
+	 * 
+	 * @param obj
+	 *            The object representing a list.
+	 * @param equivalentTerms
+	 *            The equivalent terms used to determine the equality of the
+	 *            elements of the lists.
+	 * @return Returns <code>true</code> if and only if the specified object is
+	 *         also a list, both lists have the same size, and all corresponding
+	 *         pairs of elements in the two lists are <i>equal</i> are
+	 *         equivalent according to the specified equivalent terms.
+	 */
+	public boolean equals(Object obj, IEquivalentTerms equivalentTerms);
 
 }
