1aaea2e48f17f2addc4a49125e3263f7329dd2cd
lanzanasto <lanzanasto@d19cdd9d-bb3e-0410-adf6-c6b68e32cb7f>
2012-07-03 13:32:42 +0000

    Inital import.

---
+++ streaming-iris/trunk/streaming-iris-impl/src/main/java/at/sti2/streamingiris/builtins/string/StringReplaceWithoutFlagsBuiltin.java
@@ -0,0 +1,86 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions.
+ * 
+ * Copyright (C) 2009 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package at.sti2.streamingiris.builtins.string;
+
+import static at.sti2.streamingiris.factory.Factory.BASIC;
+
+
+import at.sti2.streamingiris.EvaluationException;
+import at.sti2.streamingiris.api.basics.IPredicate;
+import at.sti2.streamingiris.api.terms.IStringTerm;
+import at.sti2.streamingiris.api.terms.ITerm;
+import at.sti2.streamingiris.builtins.FunctionalBuiltin;
+import at.sti2.streamingiris.factory.Factory;
+
+/**
+ * Represents the RIF built-in func:replace as described in
+ * http://www.w3.org/TR/xpath-functions/#func-replace, but restricts the flags
+ * to empty flags.
+ */
+public class StringReplaceWithoutFlagsBuiltin extends FunctionalBuiltin {
+
+	private static final IPredicate PREDICATE = BASIC.createPredicate(
+			"STRING_REPLACE3", 4);
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param term The terms, where the term at the first position is the
+	 *            string, the term at the second position is the regex, the term
+	 *            at the third position is the replacement and the term at the
+	 *            fourth position represents the result. The string is the
+	 *            string to apply the replace operation on. The regex is the
+	 *            regular expression. The replacement is the replacement for the
+	 *            matching substrings.
+	 * @throws IllegalArgumentException if one of the terms is {@code null}
+	 */
+	public StringReplaceWithoutFlagsBuiltin(ITerm... terms) {
+		super(PREDICATE, terms);
+	}
+
+	protected ITerm computeResult(ITerm[] terms) throws EvaluationException {
+		String string = null;
+		String regex = null;
+		String replacement = null;
+		String flags = "";
+
+		if (terms[0] instanceof IStringTerm && terms[1] instanceof IStringTerm
+				&& terms[2] instanceof IStringTerm) {
+			string = ((IStringTerm) terms[0]).getValue();
+			regex = ((IStringTerm) terms[1]).getValue();
+			replacement = ((IStringTerm) terms[2]).getValue();
+		} else {
+			return null;
+		}
+
+		String result = StringReplaceBuiltin.replace(string, regex,
+				replacement, flags);
+
+		if (result != null) {
+			return Factory.TERM.createString(result);
+		}
+
+		return null;
+	}
+
+}
---
+++ streaming-iris/trunk/streaming-iris-impl/src/test/java/at/sti2/streamingiris/functional/MagicSetsTest.java
@@ -0,0 +1,106 @@
+/*
+ * Integrated Rule Inference System (IRIS):
+ * An extensible rule inference system for datalog with extensions.
+ * 
+ * Copyright (C) 2008 Semantic Technology Institute (STI) Innsbruck, 
+ * University of Innsbruck, Technikerstrasse 21a, 6020 Innsbruck, Austria.
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA  02110-1301, USA.
+ */
+package at.sti2.streamingiris.functional;
+
+import java.util.List;
+
+import junit.framework.TestCase;
+
+
+import at.sti2.streamingiris.Configuration;
+import at.sti2.streamingiris.KnowledgeBaseFactory;
+import at.sti2.streamingiris.api.IKnowledgeBase;
+import at.sti2.streamingiris.api.basics.IQuery;
+import at.sti2.streamingiris.compiler.Parser;
+import at.sti2.streamingiris.optimisations.magicsets.MagicSets;
+import at.sti2.streamingiris.optimisations.rulefilter.RuleFilter;
+
+/**
+ * Tests for magic sets.
+ */
+public class MagicSetsTest extends TestCase {
+	public void testTranslation() throws Exception {
+		String program = "triple(?y, ?a, ?z) :- triple(?x, '88', ?y), triple(?x, ?a, ?z), NOT_EQUAL(?a, '88'), NOT_EQUAL(?x, ?y). "
+				+ "triple(?z, ?a, ?y) :- triple(?x, '88', ?y), triple(?z, ?a, ?x), NOT_EQUAL(?a, '88'), NOT_EQUAL(?x, ?y). "
+				+ "?- triple('184', ?p, ?o). ";
+		String expectedResults = "a( 1, 2 )." + "a( 0, 3 ).";
+
+		try {
+			evaluateSemiNaiveAndOptimisations(program, expectedResults);
+		} catch (Exception e) {
+		}
+		try {
+			evaluateSemiNaiveAndOptimisations(program, expectedResults);
+		} catch (Exception e) {
+		}
+		try {
+			evaluateSemiNaiveAndOptimisations(program, expectedResults);
+		} catch (Exception e) {
+		}
+		try {
+			evaluateSemiNaiveAndOptimisations(program, expectedResults);
+		} catch (Exception e) {
+		}
+		try {
+			evaluateSemiNaiveAndOptimisations(program, expectedResults);
+		} catch (Exception e) {
+		}
+		int z = 1;
+	}
+
+	public static void evaluateSemiNaiveAndOptimisations(String program,
+			String expectedResults) throws Exception {
+		Configuration configuration = KnowledgeBaseFactory
+				.getDefaultConfiguration();
+
+		configuration.programOptmimisers.add(new RuleFilter());
+		configuration.programOptmimisers.add(new MagicSets());
+
+		executeAndCheckResults(program, expectedResults, configuration,
+				"Semi-Naive and Magic Sets");
+	}
+
+	public static void executeAndCheckResults(String program, String expected,
+			Configuration configuration, String evaluationName)
+			throws Exception {
+		Parser parser = new Parser();
+		parser.parse(program);
+		List<IQuery> queries = parser.getQueries();
+
+		assert queries.size() <= 1;
+
+		IQuery query = null;
+		if (queries.size() == 1)
+			query = queries.get(0);
+
+		// Instantiate the knowledge-base
+		IKnowledgeBase kb = KnowledgeBaseFactory.createKnowledgeBase(
+				parser.getFacts(), parser.getRules(), configuration);
+
+		// Execute the query
+		if (query != null)
+			kb.execute(query);
+
+		kb.shutdown();
+	}
+}
